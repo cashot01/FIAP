3f4a840330a8b94af9d9958819328952de26cd93
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-04 08:39:09 -0300

    aula python - fiap

---
+++ python/aulaEntradaSaidaProcDados/casting.py
@@ -0,0 +1,19 @@
+valor = 56
+print(valor, type(valor))
+
+valor = 89.7
+print(valor, type(valor))
+
+valor = "cauan"
+print(valor, type(valor))
+
+# Casting - mudança de tipo de variavel
+"""
+int() - inteiro
+float() - real
+str() - texto
+bool() - logica
+"""
+
+conteudo = int(input("digite algo: ")) # input sempre le dado texto(str)
+# conteudo = int(conteudo)
---
+++ python/aulaEntradaSaidaProcDados/operadoresAritimeticos.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls") # sempre q executar o terminal fica limpo 
+"""
+adição  +
+subtração -
+multiplicação *
+divisão /
+divisão inteira //
+modulo(resto da divisão) %
+exponenciação **
+"""
+valor1 = 10
+valor2 = 3
+resposta =  valor1 - valor2
+print("resposta: {}".format(resposta))
