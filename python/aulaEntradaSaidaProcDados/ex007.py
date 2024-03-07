2f75af1a20df4b8564b97cd8d4117a9ce6f0aa3f
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-07 14:47:38 -0300

    ex007 python

--- python/aulaEntradaSaidaProcDados/ex007.py
+++ python/aulaEntradaSaidaProcDados/ex007.py
@@ -1,1 +1,9 @@
 # 7. Dada a idade de uma pessoa em dias, informar quantos dias, meses e anos ela tem (considere ano com 365 dias e mês com 30 dias). 
+import os
+os.system("cls")
+
+idadeDias = int(input("digite a idade em dias: "))
+anos = idadeDias // 365
+meses = (idadeDias % 365) // 30
+dias = (idadeDias % 365) % 30
+print(f"idade é {anos} anos, {meses} meses e {dias} dias")
