90b8906f6aa19bc749e122563d2b66f2e87e2011
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-08 15:12:17 -0300

    calculos

--- python/aulaEntradaSaidaProcDados/calculos2.py
+++ python/aulaEntradaSaidaProcDados/calculos2.py
@@ -7,6 +7,7 @@ c = 3
 delta = b ** 2 - 4*a*c
 x1 = (-b + delta**(1/2)) / 2* a
 x2 = (-b - delta**(1/2)) / 2* a
-print(f"""delta: {delta}
+print(f"""
+      delta: {delta}
       x1: {x1}
       x2: {x2}""")
