a8c558368bb61c1ab75bec3c4c8e503e0536027d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-08 15:00:39 -0300

    calculos

--- python/aulaEntradaSaidaProcDados/calculos.py
+++ python/aulaEntradaSaidaProcDados/calculos.py
@@ -4,7 +4,7 @@ b = 3
 c = 20.5
 d = 4.0
 print(f"1) {a + b * d}")
-print(f"2) {d / b - a}")
+print(f"2) {d / b - a:.2f}")
 print(f"3) {c + a % b}")
 print(f"4) {d * (a + b) // d}")
 print(f"5) {c - a % b // d}")
