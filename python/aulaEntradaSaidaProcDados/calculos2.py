0ffae701a9331d3c60c7c2640e0ed29c4e8ca99b
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-11 08:04:33 -0300

    calculos2

--- python/aulaEntradaSaidaProcDados/calculos2.py
+++ python/aulaEntradaSaidaProcDados/calculos2.py
@@ -5,9 +5,11 @@ a = -1
 b= 2
 c = 3
 delta = b ** 2 - 4*a*c
-x1 = (-b + delta**(1/2)) / 2* a
-x2 = (-b - delta**(1/2)) / 2* a
+x1 = (-b + delta**(1/2)) / (2* a)
+x2 = (-b - delta**(1/2)) / (2* a)
 print(f"""
       delta: {delta}
       x1: {x1}
       x2: {x2}""")
+
+# end = "" faz o print nao pular linha
