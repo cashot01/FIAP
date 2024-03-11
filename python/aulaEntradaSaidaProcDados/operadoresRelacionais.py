a404e718d88adee417d82d961cd97e1c1bc33b57
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-11 08:24:49 -0300

    operadores relacionais

---
+++ python/aulaEntradaSaidaProcDados/operadoresRelacionais.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+
+print("1) ", 45 == 12)
+print("2) ", 5 != 15)
+print("3) ", 34 > 77)
+print("4) ", 22 <= 33)
+print("5) ", 10 + 4 >= 2 * 5)
+print("6) ", "Cauan" == "Max")
