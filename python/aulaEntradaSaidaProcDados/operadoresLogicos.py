06718cc9970613bbabf2b3fd3783893a13ceb1b6
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-11 08:50:47 -0300

    logicos

---
+++ python/aulaEntradaSaidaProcDados/operadoresLogicos.py
@@ -0,0 +1,22 @@
+import os
+os.system("cls")
+
+"""
+Não  - not - Negação
+E    - and - Conjunção
+ou   - or  - Disjunção
+
+not > and > or
+exemplo:
+
+True and not False or False
+True and   True    or False
+True               or False
+True
+
+O not sempre inverte o estado lógico
+not True = False
+not False = True
+
+
+"""
