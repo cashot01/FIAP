c750d16c1861d04a9df2d7bbfdd7d96a062cbad9
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-13 16:05:33 -0300

    python

--- python/aulaEntradaSaidaProcDados/decisao/composta003.py
+++ python/aulaEntradaSaidaProcDados/decisao/composta003.py
@@ -2,3 +2,15 @@ import os
 os.system("cls")
 
 valor = float(input("valor da compra: R$ "))
+if valor > 500:
+    valor = valor * 0.88
+    print(f"Valor: R$ {valor:.2f} ")
+else:
+    valor = valor * 0.94
+    print(f"Valor: R$ {valor}")
+cupom = str(input("cupom: "))
+if cupom == "s":
+    valor = valor - 20
+    print(f"Valor: R$ {valor}")
+else:
+    print(f"Valor: R$ {valor}")
