7505172c15f73497c91390634a2629d3a3d62c52
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-13 11:04:49 -0300

    depositar

--- java/SistemaBancario_1TDSPI/src/Main.java
+++ java/SistemaBancario_1TDSPI/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         var novaConta = new Conta();
         novaConta.numero = "0001";
         System.out.println("o numero da conta é: "+ novaConta.numero);
+        novaConta.depositar(10.0);
         novaConta.exibirSaldo();
     }
 }
