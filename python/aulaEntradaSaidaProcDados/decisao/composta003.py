c4708bd17173d569c20076b7f2e678a7d0fd78de
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-15 10:22:53 -0300

    python

--- python/aulaEntradaSaidaProcDados/decisao/composta003.py
+++ python/aulaEntradaSaidaProcDados/decisao/composta003.py
@@ -7,10 +7,10 @@ if valor > 500:
     print(f"Valor: R$ {valor:.2f} ")
 else:
     valor = valor * 0.94
-    print(f"Valor: R$ {valor}")
+    print(f"Valor: R$ {valor:.2f}")
 cupom = str(input("cupom: "))
 if cupom == "s":
     valor = valor - 20
-    print(f"Valor: R$ {valor}")
+    print(f"Valor: R$ {valor:.2f}")
 else:
     print(f"Valor: R$ {valor}")
