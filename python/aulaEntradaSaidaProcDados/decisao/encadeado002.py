214b4b520b83f74e71d929c0053d4ca9be6ce3f4
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-15 15:01:50 -0300

    encadeado002

---
+++ python/aulaEntradaSaidaProcDados/decisao/encadeado002.py
@@ -0,0 +1,14 @@
+# 2. Dadas duas notas, calcular a média e exibir se está "aprovado" (a partir de 6), "reprovado" (menor do que 4) ou em "exame" (entre 4 e 5.9)
+import os
+os.system("cls")
+
+n1 = float(input("nota 1: "))
+n2 = float(input("nota 2: "))
+media =  (n1 + n2)/ 2
+print(f"Media: {media}")
+if media >= 6:
+    print("PASSOU")
+elif  5.9 >= media  :
+    print("EXAME")
+elif media < 4:
+    print("REPROVADO")
