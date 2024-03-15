0c07c3b6f2de053383dcf5bd842ea0eda9360325
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-15 11:52:59 -0300

    encadeado001

---
+++ python/aulaEntradaSaidaProcDados/decisao/encadeado001.py
@@ -0,0 +1,28 @@
+import os
+os.system("cls")
+
+n = int(input("nº: "))
+if n == 1:
+    print("Jan")
+elif n == 2:
+    print("Fev")
+elif n == 3:
+    print("Mar")
+elif n == 4:
+    print("Abr")
+elif n == 5:
+    print("Mai")
+elif n == 6 :
+    print("Jun")
+elif n == 7:
+    print("Jul")
+elif n == 8:
+    print("Aug")
+elif n == 9 :
+    print("Set")
+elif n == 10:
+    print("Nov")
+elif n == 11 :
+    print("")
+else n == 12 :
+    print("")
