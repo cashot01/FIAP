a4fb22210d8839b3553724d1b4442fd1af5d8bf0
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-15 22:19:52 -0300

    encadeado003

--- python/aulaEntradaSaidaProcDados/decisao/encadeado003.py
+++ python/aulaEntradaSaidaProcDados/decisao/encadeado003.py
@@ -3,55 +3,55 @@ n1 = float(input("nota 1: "))
 n2 = float(input("nota 2: "))
 n3 = float(input("nota 3: "))
 if n1 > n2 > n3:
+    maiornota = n1
+    segundanota = n2
     print(f"""
         maior nota...: {n1}
         2ª maior nota: {n2}""")
-    maiornota = n1
-    segundanota = n2
     media = (n1 + n2)/ 2
     print(f"Media: {media}")
 
 elif n1 > n3 > n2:
+    maiornota = n1
+    segundanota = n3
     print(f"""
         maior nota...: {n1}
         2ª maior nota: {n3}""")
-    maiornota = n1
-    segundanota = n3
     media = (n1 + n3)/ 2
     print(f"Media: {media}")
 
 elif n2 > n1 > n3:
+    maiornota = n2
+    segundanota = n1
     print(f"""
         maior nota...: {n2}
         2ª maior nota: {n1}""")
-    maiornota = n2
-    segundanota = n1
     media = (n2 + n1)/ 2
     print(f"Media: {media}")
 
 elif n2 > n3 > n1:
+    maiornota = n2
+    segundanota = n3
     print(f"""
         maior nota...: {n2}
         2ª maior nota: {n3}""")
-    maiornota = n2
-    segundanota = n3
     media = (n2 + n3)/ 2
     print(f"Media: {media}")
 
 elif n3 > n1 > n2:
+    maiornota = n3
+    segundanota = n1
     print(f"""
         maior nota...: {n3}
         2ª maior nota: {n1}""")
-    maiornota = n3
-    segundanota = n1
     media = (n3 + n1)/ 2
     print(f"Media: {media}")
 
 elif n3 > n2 > n1:
+    maiornota = n3
+    segundanota = n2
     print(f"""
         maior nota...: {n3}
         2ª maior nota: {n2}""")
-    maiornota = n3
-    segundanota = n2
     media = (n3 + n2)/ 2
     print(f"Media: {media}")
