f64823bc7c935f2dcc5b5893374f073f6e950b7f
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-18 09:29:14 -0300

    python

--- python/aulaEntradaSaidaProcDados/decisao/encadeado004.py
+++ python/aulaEntradaSaidaProcDados/decisao/encadeado004.py
@@ -2,8 +2,43 @@
 import os
 os.system("cls")
 n1 = float(input("nota 1: "))
-n2 = float(input("nota 2: "))
-n3 = float(input("nota 3: ")) 
+if n1 >= 0 and n1 <= 10:
+    n2 = float(input("nota 2: "))
+    if n2 >= 0 and n2 <= 10:
+        n3 = float(input("nota 3: ")) 
+        if n3 >= 0 and n3 <= 10:
+            print("NOTAS VALIDAS")
+            if n1 > n2 > n3:
+                maiornota = n1
+                segundanota = n2
+                print(f"""
+                    Maior nota...: {n1}
+                    2ª maior nota: {n2}
+                """)
+                media = (n1 + n2) / 2
+                print(f"Media: {media}")
+                if media >= 6:
+                    print("Status: PASSOU")
+                elif 5.9 >= media and media >= 4:
+                    print("Status: EXAME")
+                elif media < 4: 
+                    print("Status: REPROVADO")
+
+            elif n1 > n3 > n2:
+                maiornota = n1
+                segundanota = n3
+                print(f"""
+                    Maior nota...: {n1}
+                    2ª maior nota: {n3}
+                """)
+                media = (n1 + n3) / 2
+                print(f"Media: {media}")
+                if media >= 6:
+                    print("Status: PASSOU")
+                elif 5.9 >= media and media >= 4:
+                    print("Status: EXAME")
+                elif media < 4: 
+                    print("Status: REPROVADO")
 
 # NOTAS VALIDAS
 """if n1 >= 0 and n1 <= 10:
@@ -181,14 +216,24 @@ if (n1 >= 0 and n1 <= 10) and (n2 >= 0 and n2 <= 10) and (n3 >= 0 and n3 <= 10):
 
 
 # NOTAS INVALIDAS
+elif (n1 < 0 or n1 > 10) and (n2 < 0 or n2 > 10):
+    print(f"nota invalidas: {n1} e {n2}")
+
+elif (n1 < 0 or n1 > 10) and (n3 < 0 or n3 > 10):
+    print(f"Notas INVALIDAS: {n1} e {n3}")
+
+elif (n2 < 0 or n2 > 10) and (n3 < 0 or n3 > 10):
+    print(f"Notas INVALIDAS: {n2} e {n3}")
+
 elif n1 < 0 or n1 > 10:
-    print(f"nota invalida: {n1}")
+    print(f"Nota INVALIDA: {n1}")
 
 elif n2 < 0 or n2 > 10:
     print(f"Nota invalida: {n2}")
 
 elif n3 < 0 or n3 > 10:
     print(f"Nota invalida: {n3}")
+
 else:
     print("Notas INVALIDAS, digite notas validas (0 ate 10)")
 
