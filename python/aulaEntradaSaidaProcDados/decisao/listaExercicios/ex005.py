192a1bdf6daf94c739f341ae0409a6bdc250f44f
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-18 17:44:26 -0300

    ex5

--- python/aulaEntradaSaidaProcDados/decisao/listaExercicios/ex005.py
+++ python/aulaEntradaSaidaProcDados/decisao/listaExercicios/ex005.py
@@ -2,7 +2,7 @@
 import os 
 os.system("cls")
 n1 = float(input("nota 1: "))
-n2 = float(input("nota 2:"))
+n2 = float(input("nota 2: "))
 media = (n1 + n2) / 2
 print(f"Media: {media}")
 if media >= 5:
