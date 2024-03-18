a9fa49c6aca21dd65ac48dd55c8bb125192114fa
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-18 17:43:26 -0300

    ex5

---
+++ python/aulaEntradaSaidaProcDados/decisao/listaExercicios/ex005.py
@@ -0,0 +1,11 @@
+# 5. Dadas duas notas, calcular e exibir a média simples das mesmas. Caso a média seja inferior a 5 exibir “Você está reprovado”, senão exibir “Você está aprovado”.
+import os 
+os.system("cls")
+n1 = float(input("nota 1: "))
+n2 = float(input("nota 2:"))
+media = (n1 + n2) / 2
+print(f"Media: {media}")
+if media >= 5:
+    print("Vc passou")
+else:
+    print("REPROVADO")
