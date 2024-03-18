fe659f860625a0a34ef4c1072e446bfb44ab3fba
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-18 11:30:56 -0300

    java

--- java/SistemaBancario_1TDSPI/src/Conta.java
+++ java/SistemaBancario_1TDSPI/src/Conta.java
@@ -1,40 +1,39 @@
 public class Conta {
-    // propriedades
-    public String numero; // nº de conta não tem calculo, por isso string
+    //propriedades
+    public String numero; //0001 por ex, numero da conta não tem calculo, por isso pode ser string
+    private double saldo = 0.0; //saldo da conta, pode ser decimal, por isso double
+    private boolean chequeEspecialHabilitado = false; //cheque especial, pode ser true ou false, por isso boolean
 
-    private double saldo = 0.00;
-
-    private boolean chequeEspecialHabilitado = false;
-
-    // métodos
-
-    public double exibirSaldo(){
-
-        return saldo;
-    }
-
-    public boolean alterarStatusCheque(boolean status){
-        chequeEspecialHabilitado = status;
-        return chequeEspecialHabilitado;
-    }
-
+    //métodos ou funções
     public void depositar(double valor){
         saldo += valor;
+        System.out.println("Depósito de " + valor + " realizado com sucesso!");
     }
 
     public void sacar(double valor){
-        if(chequeEspecialHabilitado){
-            saldo -= valor;
-        }
-        // 2 barras em pé (||) - ou
-        // 2 E comercial (&&) - and
-        else if (saldo < valor || saldo == 0){
-            System.out.println("VC tá LISO!!!!");
-        }
-        else {
-            saldo -= valor;
-        }
+        // condição de cheque especial
+        if(saldo < 0)
+            System.out.println("Aviso! você está no cheque especial, " +
+                    "por isso juros poderão ser cobriados");
 
+        // condição de saque
+        if(chequeEspecialHabilitado || saldo >= valor) {
+            saldo -= valor;
+            System.out.println("\u001B[32m"+"Saque de " + valor + " realizado com sucesso!"+"\u001B[0m");
+        }
+        else
+            System.out.println("\u001B[33m"+ "Saldo insuficiente! você é um liso!!!!" + "\u001B[0m");
     }
 
+    public double exibirSaldo(){
+        return saldo;
+    }
+
+    public void habilitarChequeEspecial(){
+        chequeEspecialHabilitado = true;
+    }
+
+    public void desabilitarChequeEspecial(){
+        chequeEspecialHabilitado = false;
+    }
 }
--- java/SistemaBancario_1TDSPI/src/Main.java
+++ java/SistemaBancario_1TDSPI/src/Main.java
@@ -2,10 +2,9 @@ public class Main {
     public static void main(String[] args) {
         // criando uma conta
         var novaConta = new Conta();
+        // coloca o nº da conta
         novaConta.numero = "0001";
-        System.out.println("o numero da conta é: "+ novaConta.numero);
-        // novaConta.sacar(100);
-        System.out.println("Saldo atual: R$"+ novaConta.exibirSaldo());
+        novaConta.sacar(100);
         novaConta.depositar(1000.0);
         System.out.println("Saldo atual: R$"+novaConta.exibirSaldo());
         novaConta.sacar(100);
