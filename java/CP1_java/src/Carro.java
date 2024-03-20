7d64e39e18757a8a7354b7de1dfb1257c180361f
cashot01 <cauan.5062.asp@gmail.com>
2024-03-20 11:44:58 -0300

    java

--- java/CP1_java/src/Carro.java
+++ java/CP1_java/src/Carro.java
@@ -1,2 +1,38 @@
 public class Carro {
+    public String marca;
+
+    public int ano;
+
+    public double consumoPorKm;
+
+    public String modelo;
+
+    private double quilometragem = 0;
+
+    private double combustivelNoTanque = 0;
+
+    public double abastecer(double litros){
+        combustivelNoTanque += litros;
+        System.out.println("Tanque foi abastecido com: "+combustivelNoTanque +" l");
+        return combustivelNoTanque;
+    }
+    public void dirigir(double quilometros){
+        if (combustivelNoTanque < quilometros/consumoPorKm){
+            System.out.println("Você tem que abastecer o tanque");
+        }
+        else {
+            combustivelNoTanque -= quilometros/consumoPorKm;
+            quilometragem += quilometros;
+            System.out.println("O carro percorreu "+ quilometros +"Km");
+        }
+    }
+
+    public void previsaoDeAutonomia(){
+        double autonomia = quilometragem / combustivelNoTanque;
+        System.out.println("Autonomia de: "+ autonomia);
+    }
+
+
+
+
 }
--- java/CP1_java/src/Main.java
+++ java/CP1_java/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        var carro1 = new Carro();
+
+        carro1.ano = 2024;
+        carro1.marca = "Fiat";
+        carro1.modelo = "Argo";
+        carro1.consumoPorKm = 10;
+        carro1.abastecer(100);
+        carro1.dirigir(20);
+        carro1.previsaoDeAutonomia();
+
 
     }
 }
