9692004467eed9039db18c83101afff332cd2b53
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-25 09:24:01 -0300

    match case python

---
+++ python/aulaEntradaSaidaProcDados/match.py
@@ -0,0 +1,33 @@
+import os
+os.system("cls")
+
+
+print(f"""
+    1 - Cadastrar
+    2 - Consultar
+    3 - Alterar
+    4 - Excluir
+    """)
+opcao = int(input("Escolha: "))
+""""
+if opcao == 1:
+    print("Cadastrando....") 
+elif opcao == 2:
+    print("Consultando")
+elif opcao == 3:
+    print("Alterando")
+"""
+match opcao:
+    # case 1 | 2 | 3:  | representa o or
+    case 1:
+        print("Cadastrando...")
+    case 2: 
+        print("Consultando")
+    case 3:
+        print("Alterando")
+    case 4:
+        print("Excluindo")
+    case _:
+        print("Opção invalida")
+
+
