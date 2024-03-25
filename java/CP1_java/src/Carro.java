d9aaaff0b4601e6255d77514f868d5de90730ee7
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-25 12:02:26 -0300

    cp1 java

--- java/CP1_java/src/Carro.java
+++ java/CP1_java/src/Carro.java
@@ -1,38 +1,36 @@
 public class Carro {
     public String marca;
-
     public int ano;
-
     public double consumoPorKm;
-
     public String modelo;
+    private double combustivelNoTanque;
+    private double quilometragem;
 
-    private double quilometragem = 0;
-
-    private double combustivelNoTanque = 0;
-
-    public double abastecer(double litros){
+    public void abastecer(double litros){
         combustivelNoTanque += litros;
-        System.out.println("Tanque foi abastecido com: "+combustivelNoTanque +" l");
-        return combustivelNoTanque;
+        System.out.println("Abastecendo " + litros + "litros no tanque.");
     }
-    public void dirigir(double quilometros){
-        double combustivelNecessario = quilometros / consumoPorKm;
-        if (combustivelNoTanque < combustivelNecessario){
-            System.out.println("Você tem que abastecer o tanque");
-        }
-        else {
+
+    // Reduzir a quantidade de combustivel no tanque com base na distancia percorrida e no consumo por quilometro no carro.
+    // Aumentar a quilometragem do carro.
+    // Caso nao haja combustivel suficiente para a distancia desejada, o carro nao deve "dirigir" e deve imprimir uma mensagem indicando falta de combustivel
+    public void dirigir(double distancia){
+        double combustivelNecessario = distancia / consumoPorKm; // ex: 100km / 10km/l = 10 litros
+        if (combustivelNoTanque >= combustivelNecessario){
             combustivelNoTanque -= combustivelNecessario;
-            quilometragem += quilometros;
-            System.out.println("O carro percorreu "+ quilometros +"Km");
+            quilometragem += distancia;
+            System.out.println("Dirigindo "+ distancia +"km");
         }
+        else
+            System.out.println("Combustivel insuficiente para dirigir "+ distancia +"km");
     }
 
     public double previsaoDeAutonomia(){
-        return  combustivelNoTanque * consumoPorKm;
+        return combustivelNoTanque * consumoPorKm;
     }
-
-
-
-
 }
+
+
+
+
+
--- java/CP1_java/src/Main.java
+++ java/CP1_java/src/Main.java
@@ -1,36 +1,68 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        System.out.println("Sistema Iniciando!");
 
-        var carro1 = cadastrarCarro();
-       // carro1.ano = 2024;
-       // carro1.marca = "Fiat";
-       // carro1.modelo = "Argo";
-       // carro1.consumoPorKm = 10;
+        Menu();
 
-
-
-        carro1.abastecer(100);
-        carro1.dirigir(90);
-        System.out.println(carro1.previsaoDeAutonomia());
-
-
+        System.out.println("Sistema Finalizado!");
     }
 
     public static Carro cadastrarCarro(){
-        var carro1 = new Carro();
+        var carro = new Carro();
         System.out.println("Digite a marca do carro: ");
-        carro1.marca = scanner.nextLine();
+        carro.marca = scanner.nextLine();
         System.out.println("Digite o modelo do carro: ");
-        carro1.modelo = scanner.nextLine();
+        carro.modelo = scanner.nextLine();
         System.out.println("Digite o ano do carro: ");
-        carro1.ano = scanner.nextInt();
+        carro.ano = scanner.nextInt();
         scanner.nextLine();
-        System.out.println("digite o consumo do carro: ");
-        carro1.consumoPorKm = scanner.nextDouble();
+        System.out.println("Digite o consumo do carro: ");
+        carro.consumoPorKm = scanner.nextDouble();
         scanner.nextLine();
-        return carro1;
+        return carro;
+    }
+
+    public static void Menu(){
+        var carro = new Carro();
+        System.out.println("Bem vindo ao sistema de controle do carro!");
+
+        while (true) {
+            System.out.println("Digite a opção desejada: ");
+            System.out.println("1 - Cadastrar Carro");
+            System.out.println("2 - Abastecer Carro");
+            System.out.println("3 - Dirigir Carro");
+            System.out.println("4 - Previsão de autonomia");
+            System.out.println("5 - Sair");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1 -> carro = cadastrarCarro();
+                case 2 -> {
+                    System.out.println("Digite a quantidade de litros para abastecer: ");
+                    double litros = scanner.nextDouble();
+                    scanner.nextLine();
+                    carro.abastecer(litros);
+                }
+                case 3 -> {
+                    System.out.println("Digite a distância a ser percorrida: ");
+                    double distancia = scanner.nextDouble();
+                    scanner.nextLine();
+                    carro.dirigir(distancia);
+                }
+                case 4 ->
+                        System.out.println("A previsão de autonomia do carro é de: " + carro.previsaoDeAutonomia() + "Km");
+                case 5 -> {
+                    System.out.println("Saindo do sistema...");
+                    System.exit(0);
+                }
+                default -> System.out.println("Opção inválida!");
+            }
+        }
     }
 }
