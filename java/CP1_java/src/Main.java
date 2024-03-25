35a897a2f29b1c98ca91c4050e2ae5b162ffe4e2
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-03-25 11:36:43 -0300

    java

--- java/CP1_java/src/Main.java
+++ java/CP1_java/src/Main.java
@@ -1,19 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        var carro1 = new Carro();
-        var scanner = new Scanner(System.in);
-        System.out.println("Digite a marca do carro: ");
-        carro1.marca = scanner.nextLine();
-        System.out.println("Digite o modelo do carro: ");
-        carro1.modelo = scanner.nextLine();
-        System.out.println("Digite o ano do carro: ");
-        carro1.ano = scanner.nextInt();
-        scanner.nextLine();
-        System.out.println("digite o consumo do carro: ");
-        carro1.consumoPorKm = scanner.nextDouble();
-        scanner.nextLine();
+
+        var carro1 = cadastrarCarro();
        // carro1.ano = 2024;
        // carro1.marca = "Fiat";
        // carro1.modelo = "Argo";
@@ -26,5 +17,20 @@ public class Main {
         System.out.println(carro1.previsaoDeAutonomia());
 
 
+    }
+
+    public static Carro cadastrarCarro(){
+        var carro1 = new Carro();
+        System.out.println("Digite a marca do carro: ");
+        carro1.marca = scanner.nextLine();
+        System.out.println("Digite o modelo do carro: ");
+        carro1.modelo = scanner.nextLine();
+        System.out.println("Digite o ano do carro: ");
+        carro1.ano = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("digite o consumo do carro: ");
+        carro1.consumoPorKm = scanner.nextDouble();
+        scanner.nextLine();
+        return carro1;
     }
 }
