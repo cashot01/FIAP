c8bbfc94c284e375c6687e60ece5bb974ccd17cb
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-01 09:11:49 -0300

    repeticao

---
+++ python/aulaEntradaSaidaProcDados/repeticao/lacos.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls")
+"""
+ler o nº inicial
+ler o nº final
+exibir os nº
+somar 1 para o proximo
+"""
+inicial = int(input("valor inicial: "))
+final = int(input("valor final: "))
+
+while inicial <= final:
+    # print(f"{inicial} ", end = "") # end = "" - n pula a linha 
+    print(f"{inicial}")
+    inicial += 1
