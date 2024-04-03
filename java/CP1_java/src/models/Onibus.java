7da74902735e62c146d25d1626bf68508e6c28b0
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-03 10:59:59 -0300

    classe frota

--- java/CP1_java/src/models/Onibus.java
+++ java/CP1_java/src/models/Onibus.java
@@ -1,5 +1,49 @@
 package models;
 
 public class Onibus extends Veiculo {
-    
+    private int capacidadeDePassageiros;
+    private int passageiros;
+    public Onibus(){
+        capacidadeDePassageiros = 20;
+    }
+
+    public Onibus(String marca, int ano, double consumoPorKm, String modelo, int capacidadeDePassageiros, int passageiros) {
+        super(marca, ano, consumoPorKm, modelo);
+        this.capacidadeDePassageiros = capacidadeDePassageiros;
+        embarcarPassageiros(passageiros);
+        this.passageiros = passageiros;
+    }
+
+    public int getCapacidadeDePassageiros() {
+        return capacidadeDePassageiros;
+    }
+
+    public void setCapacidadeDePassageiros(int capacidadeDePassageiros) {
+        this.capacidadeDePassageiros = capacidadeDePassageiros;
+    }
+
+    public int getPassageiros() {
+        return passageiros;
+    }
+
+    public void setPassageiros(int passageiros) {
+
+        this.passageiros = passageiros;
+    }
+
+    public void embarcarPassageiros(int passageiros){
+        if(passageiros < 0 || passageiros > capacidadeDePassageiros){
+            System.out.println("A quantidade de passageiros não pode ser negativa \r\n" +  "ou maior qua a capacidade");
+            return;
+        }
+        this.passageiros = passageiros;
+    }
+
+    @Override
+    public String toString() {
+        return "Onibus{" +
+                "capacidadeDePassageiros=" + capacidadeDePassageiros +
+                ", passageiros=" + passageiros +
+                "} " + super.toString();
+    }
 }
