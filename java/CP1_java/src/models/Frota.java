48785040a427203202f6c9d22acf381615faafde
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-03 11:34:55 -0300

    frota

--- java/CP1_java/src/models/Frota.java
+++ java/CP1_java/src/models/Frota.java
@@ -1,5 +1,30 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frota {
-    
+    // composição é tudo qua a classe tem - ex. Frota tem Veiculos
+    private List<Veiculo> veiculos;
+    public Frota() {
+        veiculos = new ArrayList<>();
+    }
+
+    public Frota(List<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
+
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public void setVeiculos(List<Veiculo> veiculos) {
+        this.veiculos = veiculos;
+    }
+
+    public void adicionarVeiculo(Veiculo veiculo){
+        veiculos.add(veiculo);
+    }
+
+
 }
