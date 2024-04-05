376ff055a6c6b94aa264dac8f8859e957e53dae9
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-05 11:05:54 -0300

    exercicio mostrar o maior nº

--- python/aulaEntradaSaidaProcDados/repeticao/cont_break_else3.py
+++ python/aulaEntradaSaidaProcDados/repeticao/cont_break_else3.py
@@ -5,4 +5,14 @@ digite 10 numeros:
  
 o maior  100
 """
+maior = 0
+volta = 1
+print("digite 10 nº: ")
 
+while volta <= 10:
+    num = int(input())
+    if num > maior:
+        maior = num
+    volta += 1
+    
+print(f"Maior: {maior}")
