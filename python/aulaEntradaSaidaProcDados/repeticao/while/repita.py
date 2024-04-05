b8d50a30c2c8aad2cf95cc04342e6d5161d1256e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-05 11:48:13 -0300

    repita

---
+++ python/aulaEntradaSaidaProcDados/repeticao/while/repita.py
@@ -0,0 +1,20 @@
+import os
+os.system("cls")
+
+while True:
+    print("vou arrasar no cp2")
+    opcao = input("Continuar, <s>im ou <n>ão? ")
+    if opcao == "n":
+        break
+
+
+
+
+# opcao = "s"
+
+# while opcao == "s":
+#     print("vou arrasar no cp2")
+#     opcao = input("Continuar, <s>im ou <n>ão? ")
+
+
+
