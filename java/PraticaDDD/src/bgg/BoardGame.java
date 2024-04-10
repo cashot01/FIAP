c47bfaff13a63ac575d8448a7cbb461bcb70071c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-10 10:35:45 -0300

    boardgame java

---
+++ java/PraticaDDD/src/bgg/BoardGame.java
@@ -0,0 +1,12 @@
+package bgg;
+
+public class BoardGame {
+    private String name;
+    private int yearlaunched;
+    private int minPlayers;
+    private int maxPlayers;
+    private int bestNumPlayers;
+    private int meanPlayTime;
+    private int minAge;
+    
+}
