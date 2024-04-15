26c8969fab15c295d8ebc021413ad0b27ec46284
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-15 09:48:06 -0300

    ex teste cp2 python

--- python/aulaEntradaSaidaProcDados/repeticao/for/exTesteCp2.py
+++ python/aulaEntradaSaidaProcDados/repeticao/for/exTesteCp2.py
@@ -28,80 +28,97 @@ Relatório:
 """
 import os
 os.system("cls")
+# inicializando variaveis contatadoras
 reprovado = 0
 aprovado = 0
 media_maxima = 0
 
-print("----------Checkpoint----------")
-cp1 = float(input("Checkpoint 1: "))
-while cp1 < 0 or cp1 > 10:
-    print("Erro! Digite uma nota valida")
+# iniciando laço com 10 alunos
+for i in range(1, 4, 1):
+    os.system("cls")
+
+    # Digitando Checkpoint
+    print(f"Aluno {i}")
+    print("----------Checkpoint----------")
     cp1 = float(input("Checkpoint 1: "))
-cp2 = float(input("Checkpoint 2: "))
-while cp2 < 0 or cp2 > 10:
-    print("Erro! Digite uma nota valida")
+    while cp1 < 0 or cp1 > 10:
+        print("Erro! Digite uma nota valida")
+        cp1 = float(input("Checkpoint 1: "))
     cp2 = float(input("Checkpoint 2: "))
-cp3 = float(input("Checkpoint 3: "))
-while cp3 < 0 or cp3 > 10:
-    print("Erro! Digite uma nota valida")
+    while cp2 < 0 or cp2 > 10:
+        print("Erro! Digite uma nota valida")
+        cp2 = float(input("Checkpoint 2: "))
     cp3 = float(input("Checkpoint 3: "))
+    while cp3 < 0 or cp3 > 10:
+        print("Erro! Digite uma nota valida")
+        cp3 = float(input("Checkpoint 3: "))
 
-menor = cp1
-if cp2 < menor:
-    menor = cp2
-if cp3 < menor:
-    menor = cp3
-media_Cp = ((cp1 + cp2 + cp3) - menor) / 2
-proporcao_Cp = media_Cp * 0.2
+    menor = cp1
+    if cp2 < menor:
+        menor = cp2
+    if cp3 < menor:
+        menor = cp3
+    media_Cp = ((cp1 + cp2 + cp3) - menor) / 2
+    proporcao_Cp = media_Cp * 0.2
 
 
-
-print("----------Challenge----------")
-sp1 = float(input("Sprint 1: "))
-nota_invalida = sp1 < 0 or sp1 > 10
-
-while nota_invalida:
-    print("Erro digite uma nota valida")
+    # Digitando Challenges
+    print("----------Challenge----------")
     sp1 = float(input("Sprint 1: "))
     nota_invalida = sp1 < 0 or sp1 > 10
 
-sp2 = float(input("Sprint 2: "))
-nota_invalida = sp2 < 0 or sp2 > 10
+    while nota_invalida:
+        print("Erro digite uma nota valida")
+        sp1 = float(input("Sprint 1: "))
+        nota_invalida = sp1 < 0 or sp1 > 10
 
-while nota_invalida:
-    print("Erro digite uma nota valida")
     sp2 = float(input("Sprint 2: "))
     nota_invalida = sp2 < 0 or sp2 > 10
 
-media_challenge = (sp1 + sp2) / 2
-proporcao_challenge = media_challenge * 0.2
+    while nota_invalida:
+        print("Erro digite uma nota valida")
+        sp2 = float(input("Sprint 2: "))
+        nota_invalida = sp2 < 0 or sp2 > 10
 
+    media_challenge = (sp1 + sp2) / 2
+    proporcao_challenge = media_challenge * 0.2
 
-print("\n-------Global Solution----------------")
-gs = float(input("Global Solution 1: "))
-while gs < 0 or gs > 10:
-    print("Erro digite uma nota valida")
+    # Digitando Global Solution
+    print("\n-------Global Solution----------------")
     gs = float(input("Global Solution 1: "))
-proporcao_gs = gs * 0.6
+    while gs < 0 or gs > 10:
+        print("Erro digite uma nota valida")
+        gs = float(input("Global Solution 1: "))
+    proporcao_gs = gs * 0.6
 
-print("\n============= M Ê D I A S =====================")
-print(f"Checkpoint........: {media_Cp}")
+    # exibindo as medias
+    print("\n============= M Ê D I A S =====================")
+    print(f"Checkpoint........: {media_Cp}")
 
-print(f"Challenge.........: {media_challenge}")
+    print(f"Challenge.........: {media_challenge}")
 
-print(f"Global Solution...: {gs}")
+    print(f"Global Solution...: {gs}")
 
-media_final = (proporcao_Cp + proporcao_challenge + proporcao_gs)
+    # definição de status de cada aluno
+    media_final = (proporcao_Cp + proporcao_challenge + proporcao_gs)
+    print(f"\n=====> MÉDIA FINAL {media_final:.1f}", end= " ")
+    if media_final < 6:
+        print(f"Resprovado\n")
+    else:
+        print(f"Aprovado\n")
 
-print(f"\n=====> MÉDIA FINAL {media_final:.1f}", end= " ")
-if media_final < 6:
-    print(f"Resprovado\n")
-else:
-    print(f"Aprovado\n")
+    input("digite algo para continuar. . .")
 
-if media_final < 6:
-    reprovado += 1
-elif media_final >= 6:
-    aprovado += 1
-    if media_final == 10:
-        media_maxima += 1
+    # consolidação dos dados do relatorio
+    if media_final < 6:
+        reprovado += 1
+    elif media_final >= 6:
+        aprovado += 1
+        if media_final == 10:
+            media_maxima += 1
+
+# exibição do relatorio final
+os.system("cls")
+print(f"Aprovados....: {aprovado}")
+print(f"Reprovados...: {reprovado}")
+print(f"Média Maxima..: {media_maxima}")
