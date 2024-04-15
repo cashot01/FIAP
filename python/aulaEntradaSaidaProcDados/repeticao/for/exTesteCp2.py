1a07248ea793e65be7ac5b21f53d1f8a9cde3375
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-15 08:01:24 -0300

    ex teste cp2 python

---
+++ python/aulaEntradaSaidaProcDados/repeticao/for/exTesteCp2.py
@@ -0,0 +1,30 @@
+"""
+EXERCÍCIO COMPLETO
+Uma turma tem 10 alunos.
+Um professor desenvolveu uma rotina para ter um melhor gerenciamento o
+dos alunos.
+A média anual se dá por:
+- Checkpoint:
+    - São 3 avaliações
+    - Calcula-se a média comum das duas maiores notas
+    - Vale 20% da média final
+- Challenge:
+    - São 2 sprints
+    - O cálculo é a média simples das duas notas
+    - Vale 20% da média final
+- Global solution:
+    - Apenas uma nota que
+    - Vale 60% da média final.
+Requisitos:
+- A média final para passar é ao menos 6, senão estará reprovado
+- Toda vez que for digitada uma nota inválida (fora de 0 e 10), advertir
+  e pedir novamente a digitação da mesma nota.
+Relatório:
+- para cada aluno exibir as médias calculadas e se ele está aprovado ou não.
+- no final da digitação das notas dos dez alunos, exibir:
+    - quantos foram aprovados
+    - quantos foram reprovados
+    - quantos tiraram a media final maxima (nota 10)
+"""
+import os
+os.system("cls")
