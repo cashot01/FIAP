e97d3452064233e8f78756f8da2f2dae4703f9fd
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-15 07:49:44 -0300

    for ex2 python

--- python/aulaEntradaSaidaProcDados/repeticao/for/forex2.py
+++ python/aulaEntradaSaidaProcDados/repeticao/for/forex2.py
@@ -1,11 +1,13 @@
 # peça 2 nº exiba em crescente se n1 > n2 ou decrescente se n2> n1
 
-n1 = int(input("n1: "))
-n2 = int(input("n2: "))
+import os
+os.system("cls")
+num1 = int(input('Inicio: '))
+num2 = int(input('Fim: '))
 
-if (n1 <= n2):
-    for crescente in range(n1, n2 + 1, 1):
-        print(crescente)
+if num1 <= num2:
+    for i in range(num1, num2 + 1, 1):
+        print(i, end = ' ')
 else:
-    for decrescente in range(n1, n2 -1 , -1):
-        print(decrescente)
+    for i in range(num1, num2 -1, -1):
+        print(i, end = ' ')
