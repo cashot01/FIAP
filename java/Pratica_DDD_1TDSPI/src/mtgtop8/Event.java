abecb70839b8313da5f0adf822f5b9b205d6e74c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-17 11:25:02 -0300

    java

--- java/Pratica_DDD_1TDSPI/src/mtgtop8/Event.java
+++ java/Pratica_DDD_1TDSPI/src/mtgtop8/Event.java
@@ -10,4 +10,76 @@ public class Event {
     private List<TournamentPlayer> players;
     private int rating;
     private String location;
+
+    public Event() {
+    }
+
+    public Event(String name, String format, LocalDateTime data, List<TournamentPlayer> players, int rating, String location) {
+        this.name = name;
+        this.format = format;
+        this.data = data;
+        this.players = players;
+        this.rating = rating;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public List<TournamentPlayer> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<TournamentPlayer> players) {
+        this.players = players;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "name='" + name + '\'' +
+                ", format='" + format + '\'' +
+                ", data=" + data +
+                ", players=" + players +
+                ", rating=" + rating +
+                ", location='" + location + '\'' +
+                '}';
+    }
 }
