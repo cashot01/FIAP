ecfb025bd5ef59da8d333851e0dac44357819710
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-17 11:13:25 -0300

    pratica ddd java

---
+++ java/Pratica_DDD_1TDSPI/src/mtgtop8/CARD_TYPE.java
@@ -0,0 +1,12 @@
+package mtgtop8;
+
+public enum CARD_TYPE {
+    CREATURE,
+    ARTIFACT,
+    ENCHANTIMENT,
+    INSTANT,
+    SORCERY,
+    PLANESWALKER,
+    LAND,
+    OTHER,
+}
--- java/Pratica_DDD_1TDSPI/src/mtgtop8/Card.java
+++ java/Pratica_DDD_1TDSPI/src/mtgtop8/Card.java
@@ -1,4 +1,13 @@
 package mtgtop8;
+
+import java.util.List;
+
 public class Card {
-    
+    private String name;
+    private Card type;
+    private String oracleText;
+    private String manaCoast;
+    private String imageUrl;
+    private List<Price> prices;
+
 }
