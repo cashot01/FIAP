cab9d34611ed8be48a22678fda22f6d311a98f0c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-19 10:54:13 -0300

    vetor

---
+++ python/aulaEntradaSaidaProcDados/vetor/vetor.py
@@ -0,0 +1,8 @@
+# em python não existe vetor , simulamos vetor 
+#     0   1   2   3   4     <= INDICES
+v = [45, 89, 32, 12, 33]
+print(f"Exibe a posição 2 do vetor: v[2] = {v[2]}")
+print(f"Exibe o vetor inteiro: V = {v}")
+# mudando a primeira posição para 10
+v[0] = 10
+print(f"V = {v}")
