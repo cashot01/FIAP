89e1bbf9f32a6bfc2f442a7014322f370dbcfc33
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-22 08:26:44 -0300

    subalgoritimos

--- python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
+++ python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
@@ -1,1 +1,25 @@
-# 
+# Calcular a equação de 2 grau por baskara
+# Subalgoritimos - Funções e Procedimentos ===> Métodos
+import os
+os.system("cls")
+
+# ========================== SUBALGORITIMOS
+# Procedimento ==> Subalgoritimo que n retorna valor ao programa chamador
+
+
+
+
+# Funções ==> Subalgoritimo que retorna valor ao programa chamador
+def calcular_delta(aa, bb, cc): # aa -1 | bb 2 | cc 3
+    resp = bb ** 2 - 4 * aa * cc
+    return resp
+
+
+
+# =========================  PROGRAMA PRINCIPAL
+a = float(input("a: ")) # -1
+b = float(input("b: ")) # 2
+c = float(input("c: ")) # 3
+
+delta =  calcular_delta(a, b, c)
+print(delta)
