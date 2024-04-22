20abd8e96b217b5d83fa83f539d609c9d1ec0175
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-22 09:40:46 -0300

    exemplo subalgoritimos

---
+++ python/aulaEntradaSaidaProcDados/subalgoritimos/exemplo.py
@@ -0,0 +1,23 @@
+"""
+Exercicios
+1 -  criar uma função chamada "maior2n" que passe por 2 nº por parametro e retorne o maior 
+|   x = maior2n(5, 9)
+>> x valerá 9
+
+2 - criar uma função chamada "maior2n" que passe por 3 nº por parametro e retorne o maior 
+|   x = maior2n(5, 9, 12)
+>> x valerá 12
+
+3 - Criar um procedimento chamado "ordena2ncrescente" que passe 2 nº por parametro e os exiba em ordem crescente
+| ordena2ncrescente(12, 9)
+>> exibirá na tela 9 e 12
+
+4 - Criar um procedimento chamado "ordena2ndecrescente" que passe 2 nº por parametro e os exiba em ordem crescente
+| ordena2ndecrescente(12, 9)
+>> exibirá na tela 12 e 9
+
+5 - crie um procedimento chamado "ordena2n", similar aos anteriores, onde outro parametro defina se é crescente ou decrescente 
+| ordena2n(34, 5, "c") # c - crescente | d - decrescente
+>> Crescente 5 e 34
+
+"""
