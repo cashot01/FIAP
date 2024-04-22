dae84feb90a7a1437d0c96ee60e84896fd707d8e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-22 08:53:11 -0300

    subalgoritimos

--- python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
+++ python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
@@ -1,36 +1,33 @@
-# Calcular a equação de 2 grau por baskara
 # Subalgoritimos - Funções e Procedimentos ===> Métodos
 import os
 os.system("cls")
+import math
 
 # ========================== SUBALGORITIMOS
 # Procedimento ==> Subalgoritimo que n retorna valor ao programa chamador
+def saudacao1():
+    print("Bom dia usuário")
 
+def saudacao2(nome):
+    print(f"Bom dia {nome} ")
+
+def saudacao3(nome, hora):
+    if hora < 12:
+        msg =  "bom dia"
+    elif hora < 18:
+        msg =  "boa tarde"
+    else:
+        msg =  "boa noite"
+    print(f"{msg}, {nome}")
 
 
 
 # Funções ==> Subalgoritimo que retorna valor ao programa chamador
-def calcular_delta(aa, bb, cc): # aa -1 | bb 2 | cc 3
-    resp = bb ** 2 - 4 * aa * cc
-    return resp
 
-def calcular_x1(aa, bb, cc):
-    resp = ((-b + calcular_delta(aa, bb, cc) ** (1/2)) / (2 * a))
-    return resp
 
 
 
 # =========================  PROGRAMA PRINCIPAL
-a = float(input("a: ")) # -1
-b = float(input("b: ")) # 2
-c = float(input("c: ")) # 3
-
-delta =  calcular_delta(a, b, c)
-# print(delta)
-
-if delta < 0:
-    print("Não é possivel calcular as raizes")
-else:
-    x1 = calcular_x1(a, b, c)
-    x2 = ((-b - delta ** (1/2)) / (2 * a))
-    print(f"x1 = {x1} | x2 = {x2}")
+saudacao1()
+saudacao2("Cauan") # "Cauan" é um parametro
+saudacao3("gustavo", 17)
