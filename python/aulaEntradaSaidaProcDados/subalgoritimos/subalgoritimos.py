5874994d4179461e31847d312c5ef01c7a7713fd
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-22 09:27:45 -0300

    subalgoritimos

--- python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
+++ python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
@@ -1,19 +1,35 @@
 # Subalgoritimos - Funções e Procedimentos ===> Métodos
-import os
+from sub  import saudacao1, saudacao2, saudacao3, raiz2, raizn
+# from sub import importa somente as funções que eu quero
+# quando for chamar a função n precisa do sub., apenas coloque o nome da função  por exemplo saudacao1()
+saudacao1()
+saudacao2()
+saudacao3()
+
+
+
+
+
+
+
+
+
+
+"""
+# Subalgoritimos - Funções e Procedimentos ===> Métodos
 import sub
-os.system("cls")
+# criei uma bibilioteca chamada sub em outro arquivo
 
 
-
-# =========================  PROGRAMA PRINCIPAL
 sub.saudacao1()
 sub.saudacao2("Cauan") # "Cauan" é um parametro
 sub.saudacao3("gustavo", 17)
 
-print(math.sqrt(16))
-print(raiz2(16))
-print(raizn(27, 3))
-print(pi())
+print(sub.raiz2(16))
+print(sub.raizn(27, 3))
+print(sub.pi())
 r = 2
-c = 2 * pi() * r
+c = 2 * sub.pi() * r
 print(f"raio = {r}, circunferencia = {c}")
+
+"""
