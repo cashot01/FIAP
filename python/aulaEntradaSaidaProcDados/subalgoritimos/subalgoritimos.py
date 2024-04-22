183e4b10d7858c323e67042160dc2885abc44b2e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-22 08:39:14 -0300

    subalgoritimos

--- python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
+++ python/aulaEntradaSaidaProcDados/subalgoritimos/subalgoritimos.py
@@ -14,6 +14,10 @@ def calcular_delta(aa, bb, cc): # aa -1 | bb 2 | cc 3
     resp = bb ** 2 - 4 * aa * cc
     return resp
 
+def calcular_x1(aa, bb, cc):
+    resp = ((-b + calcular_delta(aa, bb, cc) ** (1/2)) / (2 * a))
+    return resp
+
 
 
 # =========================  PROGRAMA PRINCIPAL
@@ -22,4 +26,11 @@ b = float(input("b: ")) # 2
 c = float(input("c: ")) # 3
 
 delta =  calcular_delta(a, b, c)
-print(delta)
+# print(delta)
+
+if delta < 0:
+    print("Não é possivel calcular as raizes")
+else:
+    x1 = calcular_x1(a, b, c)
+    x2 = ((-b - delta ** (1/2)) / (2 * a))
+    print(f"x1 = {x1} | x2 = {x2}")
