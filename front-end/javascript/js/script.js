ddab04f0df55824ee76e2166f7c8072a63cb62ee
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-23 09:51:09 -0300

    javascript

--- front-end/javascript/js/script.js
+++ front-end/javascript/js/script.js
@@ -9,9 +9,45 @@ if(true){
     
 }
 
-let nome = "Cauan"
+const curso = "Javascript"
+// const - variavel constante,  não muda o valor dela
+console.log(curso);
 
+let nome = "Cauan" // String
+// let - variavel 
 console.log(nome)
+
+let preco = 17.35 // Number
+console.log(preco);
+
+let quant = 53 // Number
+console.log(quant);
+
+let casado = true // Boolean
+console.log(casado);
+
+let frutas = ["Maça", "Banana", "Uva"]
+console.log(frutas);
+console.log(frutas[0]);
+
+let carro = {
+    cor: "Preta",
+    marca: "Honda",
+    modelo: "Civic",
+    portas: 4,
+    correr: function(){
+        alert("Estou correndo")
+    }
+}
+
+console.log(carro)
+console.log(carro.cor)
+console.log(carro.marca)
+
+carro.correr()
+
+
+
 
 // ; é opcional
 
