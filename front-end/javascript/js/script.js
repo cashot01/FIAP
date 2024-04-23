f4a2a1d43bccb4bb8abde2c92a9fb53d9aae8c20
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-04-23 09:32:57 -0300

    javascript

---
+++ front-end/javascript/js/script.js
@@ -0,0 +1,21 @@
+
+// alert("Terceiro olá mundo!!!!")
+// comentario 1 linha 
+/*
+comentario varias linhas  (barra * * barra)
+*/ 
+
+if(true){
+    
+}
+
+let nome = "Cauan"
+
+console.log(nome)
+
+// ; é opcional
+
+
+
+// document.write(nome)
+
