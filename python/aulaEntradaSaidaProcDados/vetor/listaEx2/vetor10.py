14de21733def9c11869f321e6090fba3a2f06b8d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-02 14:36:36 -0300

    vetor

--- python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor10.py
+++ python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor10.py
@@ -1,5 +1,6 @@
 # exercicio 10 - fazer procedimento copia_vetor, onde copia o vetor 1 no vetor2
-
+import os
+os.system("cls")
 def copia_vetor(v1:list, v2:list) -> int:
     v2 = [v1[0], v1[1], v1[2], v1[3], v1[4]]
     print(f"""
---
+++ python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor11.py
@@ -0,0 +1,7 @@
+# exercicio inverte_vetor , onde o v2 recebe o v1 ao contrário
+import os
+os.system("cls")
+
+
+v1 = [41, 72, 39, 4, 35]
+v2 = [0, 0, 0, 0, 0]
