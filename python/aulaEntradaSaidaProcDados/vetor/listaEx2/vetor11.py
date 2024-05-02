71b270db155e35c377696c73be59bfa0dc45a45a
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-02 14:39:45 -0300

    vetor11 python

--- python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor11.py
+++ python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor11.py
@@ -2,6 +2,20 @@
 import os
 os.system("cls")
 
+def inverte_vetor(v1:list, v2:list) -> int:
+    v2 = [v1[4], v1[3], v1[2], v1[1], v1[0]] 
+    print(f"""
+    vetor 1: {v1}
+    vetor 2: {v2}""")
+
+
+
 
 v1 = [41, 72, 39, 4, 35]
 v2 = [0, 0, 0, 0, 0]
+print(f"""
+    vetor 1: {v1}
+    vetor 2: {v2}""")
+
+inverte_vetor(v1, v2)
+
