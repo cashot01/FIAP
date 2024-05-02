05942895807a25e447181c29a2e0ac04131a007f
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-02 14:59:01 -0300

    vetor13

---
+++ python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor13.py
@@ -0,0 +1,15 @@
+# exercicio ordena_ordem_decrescente, ordem drecrescente
+import os
+os.system("cls")
+
+def ordena_ordem_decrescente(v1:list) :
+    v1 = [v1[1], v1[0], v1[2], v1[4], v1[3]]
+    print(f"\n Ordem Decrescente: {v1}")
+
+
+
+v1 = [41, 72, 39, 4, 35]
+print(v1)
+
+ordena_ordem_decrescente(v1)
+
