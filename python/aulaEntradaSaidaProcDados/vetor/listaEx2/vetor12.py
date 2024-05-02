bc4d219dde9486b15fdaebf5b90038bd22b2f17a
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-02 14:54:23 -0300

    vetro12

---
+++ python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor12.py
@@ -0,0 +1,15 @@
+# exercicio ordena_ordem_crescente, coloca em ordem crescente
+import os
+os.system("cls")
+
+def ordena_ordem_crescente(v1:list) :
+    v1 = [v1[3], v1[4], v1[2], v1[0], v1[1]]
+    print(f"""\n ordem Crescente: 
+            {v1}""")
+
+
+#      0,  1,  2,  3, 4
+v1 = [41, 72, 39, 4, 35]
+print(f"{v1}")
+
+ordena_ordem_crescente(v1)
