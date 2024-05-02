fa8a95e6e99502c8aa7f1c23e6a03486374b034d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-02 14:34:16 -0300

    vetor10 python

---
+++ python/aulaEntradaSaidaProcDados/vetor/listaEx2/vetor10.py
@@ -0,0 +1,19 @@
+# exercicio 10 - fazer procedimento copia_vetor, onde copia o vetor 1 no vetor2
+
+def copia_vetor(v1:list, v2:list) -> int:
+    v2 = [v1[0], v1[1], v1[2], v1[3], v1[4]]
+    print(f"""
+    vetor 1: {v1}
+    vetor 2: {v2}""")
+
+
+
+
+v1 = [41, 72, 39, 4, 35]
+v2 = [0, 0, 0, 0, 0]
+print(f"""
+    vetor 1: {v1}
+    vetor 2: {v2}""")
+
+copia_vetor(v1, v2)
+
