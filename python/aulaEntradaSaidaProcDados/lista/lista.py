049570130f5815edeee0e9b039c5e231a9579f75
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-03 11:41:19 -0300

    lista

---
+++ python/aulaEntradaSaidaProcDados/lista/lista.py
@@ -0,0 +1,11 @@
+lista = list()
+lista.append(22)
+# .append adiciona no final da lista
+lista.append("Logica")
+print(lista)
+
+lista.insert(10, "Cauan")
+# insert insere um dado numa posição da lista especifica
+# 1º coloca o indice depois o dado
+# se o valor do indice for maior que a lista, o item será colocado no final da lista
+print(lista)
