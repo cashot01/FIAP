48cf33a821d9a6377761555dd63f302cc7a24160
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-03 12:03:03 -0300

    lista python

--- python/aulaEntradaSaidaProcDados/lista/lista.py
+++ python/aulaEntradaSaidaProcDados/lista/lista.py
@@ -1,6 +1,7 @@
 lista = list()
 lista.append(22)
 # .append adiciona no final da lista
+lista.append(57.7)
 lista.append("Logica")
 print(lista)
 
@@ -9,3 +10,17 @@ lista.insert(10, "Cauan")
 # 1º coloca o indice depois o dado
 # se o valor do indice for maior que a lista, o item será colocado no final da lista
 print(lista)
+
+lista.pop(1)
+# pop remove o elemento com a posição informada da lista, caso não informe a posição remove o ultimo elemento
+# se colocar indice q 
+print(lista)
+
+lista.remove(57.7)
+# remove o elemento 
+# se colocar elemento q n existe da erro
+print(lista)
+
+indice = lista.index("Cauan")
+print(f"Indice = {indice}")
+
