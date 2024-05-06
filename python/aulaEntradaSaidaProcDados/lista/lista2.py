c2a71dd33e8655b8d15fa414dc49958a45284738
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-06 09:46:22 -0300

    lista2 python

--- python/aulaEntradaSaidaProcDados/lista/lista2.py
+++ python/aulaEntradaSaidaProcDados/lista/lista2.py
@@ -5,27 +5,19 @@ os.system("cls")
 0-45 | 1-4.6 | 2-Edson | 3-a | 4-False 
 """
 
-lista = list()
+#============================= SUBALGORITIMO
+
+def exibe_lista(l: list) -> None:
+    for i in range(0, len(lista), 1):
+        print(f"{i}-{lista[i]} | ", end=" ")
+
+
+
+# ====================== PROGRAMA PRINCIPAL
+
+lista = [45, 4.6, "Edson", "a", False]
 print(lista)
 
-for i in range(0, 5, 1):
-    elemento = input("digite algo: ")
-    lista.append(elemento)
-
-indice0 = lista.index(lista[0])
+exibe_lista(lista)
 
 
-indice1 = lista.index(lista[1])
-
-
-indice2 = lista.index(lista[2])
-
-
-indice3 = lista.index(lista[3])
-
-
-indice4 = lista.index(lista[4])
-
-print(lista)
-
-print(f"\n {indice0} - {lista[0]} | {indice1} - {lista[1]} | {indice2} - {lista[2]} | {indice3} - {lista[3]} | {indice4} - {lista[4]}")
