eb3f74d94aae962c7ce612bf2eb6118eaecc28ba
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-10 09:46:10 -0300

    lista6 python

---
+++ python/aulaEntradaSaidaProcDados/lista/lista6.py
@@ -0,0 +1,21 @@
+# 6 - conta_string() conta quantas strings tem em uma lista
+import os
+os.system("cls")
+
+lista = ["eds", 56, "fiap", "ester", True, 45.78, 12, 78]
+print(lista)
+print(type(lista))
+# type(lista[0])
+print(f"{type(lista[0])}, {type(lista[1])} , {type(lista[2])}, {type(lista[3])}, {type(lista[4])}, {type(lista[5])}, {type(lista[6])} ")
+
+if type(lista) == "list":
+    qtd_string = lista.count(str)
+    print(qtd_string)
+
+# for i in range(len(lista)):
+#     if type((lista)) == str:
+#         qtd_strings = lista.count(lista(str))
+#         qtd = len(lista)
+#         print(len(lista))
+#         print(qtd_strings)
+
