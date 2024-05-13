87f8f5e366c88b75ed52158b1cae4a8f7aa43520
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-13 08:51:43 -0300

    tupla

---
+++ python/aulaEntradaSaidaProcDados/fatiamentoStrings/strings.py
@@ -0,0 +1,31 @@
+import os
+os.system("cls")
+
+# cada caracter de uma string pode ser tratada com um elemento
+"""
+frase = "Dia 29 é aniversário do Edson"
+print(frase)
+print(frase[5])
+"""
+
+"""
+Tratando Indices positivos e negativos
+
+#         0   1   2   3   4   5   6   7   8 <== indice positivo
+lista = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+#        -9  -8  -7  -6  -5  -4  -3  -2  -1 <== indice negativo
+
+print(lista[2])
+# quado quiser printar o ultimo elemento da lista digite -1
+print(lista[-1])
+print(lista[2:5]) # inicio : fim (pega do indice 2 até o 4 )
+pedaco_lista = lista[3:7]
+print(pedaco_lista)
+print(lista[-8:-4])
+print(lista[0:9])
+print(lista[0:9:2]) # inico : fim : passo (mostra a lista inteira pulando de 2 em 2)
+print(lista[::3]) # pula 3 em 3 a lista toda
+print(lista[::-2])
+"""
+
+
---
+++ python/aulaEntradaSaidaProcDados/fatiamentoStrings/tupla.py
@@ -0,0 +1,13 @@
+# Tupla é uma "lista" que não pode ser interada (manipulada / modificada), seria como uma lista
+
+import os
+os.system("cls")
+
+lista = [4, 5, 6]
+print(lista)
+lista[0] = 99
+print(lista)
+
+tupla = (65, 34, 78, 29, "aniversario", "edson")
+# tupla é entre parenteses
+print(tupla)
