99148749f92eb73ebf6a9a3d3875c1d1beab179e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-13 07:54:23 -0300

    lista6

--- python/aulaEntradaSaidaProcDados/lista/lista6.py
+++ python/aulaEntradaSaidaProcDados/lista/lista6.py
@@ -2,14 +2,14 @@
 import os
 os.system("cls")
 
-lista = ["eds", 56, "fiap", "ester", True, 45.78, 12, 78]
+lista = ["eds", "56", "fiap", "ester", "True", "45.78", "12", "78" ]
 print(lista)
 print(type(lista))
 # type(lista[0])
 print(f"{type(lista[0])}, {type(lista[1])} , {type(lista[2])}, {type(lista[3])}, {type(lista[4])}, {type(lista[5])}, {type(lista[6])} ")
 
 if type(lista) == "list":
-    qtd_string = lista.count(str)
+    qtd_string = lista.count()
     print(qtd_string)
 
 # for i in range(len(lista)):
