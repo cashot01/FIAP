d4026035b1e2602e35b5bbbe7de4c3d8b48eff84
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-13 09:00:28 -0300

    fatiamento

---
+++ python/aulaEntradaSaidaProcDados/fatiamento/fatiamento_strings.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+frase = "O palmeiras e o corinthias perderam"
+print("1. " + frase)
+print("2. " + frase[2])
+print("3. " + frase[7:15])
+print("4. " + frase[::-1])
+print("5. " + frase[5:25:2])
+print("6. " + frase[30:3:-5])
