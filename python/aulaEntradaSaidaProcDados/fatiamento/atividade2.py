37fb5a64e0981ba6dbbba635013fc658d8d7acc8
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-17 11:53:33 -0300

    atividade

--- python/aulaEntradaSaidaProcDados/fatiamento/atividade2.py
+++ python/aulaEntradaSaidaProcDados/fatiamento/atividade2.py
@@ -4,6 +4,7 @@ def eh_numero(numero: str) -> bool:
     digitos = "0123456789"
     if numero[0] == "+" or numero[0] == "-":
         for i, carac in enumerate(numero):
+            
 
             if carac in digitos:
                 return numero
