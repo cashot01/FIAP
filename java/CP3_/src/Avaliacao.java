ed3a2968c73c40514d1e402adf032e771ca9de75
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-22 10:38:23 -0300

    cp3

--- java/CP3_/src/Avaliacao.java
+++ java/CP3_/src/Avaliacao.java
@@ -1,47 +1,50 @@
 public class Avaliacao {
-    private int pontuacao;
-    private String comentario;
-    private String nomeUsuario;
+        private String usuario;
+        private int nota;
+        private String comentario;
 
-    public Avaliacao() {
+        public Avaliacao(){
+
+        }
+
+        public Avaliacao(String usuario, int nota, String comentario) {
+            this.usuario = usuario;
+            this.nota = nota;
+            this.comentario = comentario;
+        }
+
+        public String getUsuario() {
+            return usuario;
+        }
+
+        public void setUsuario(String usuario) {
+            this.usuario = usuario;
+        }
+
+        public int getNota() {
+            return nota;
+        }
+
+        public void setNota(int nota) {
+            this.nota = nota;
+        }
+
+        public String getComentario() {
+            return comentario;
+        }
+
+        public void setComentario(String comentario) {
+            this.comentario = comentario;
+        }
+
+        @Override
+        public String toString() {
+            return "Avaliacao{" +
+                    "usuario='" + usuario + '\'' +
+                    ", nota=" + nota +
+                    ", comentario='" + comentario + '\'' +
+                    '}';
+        }
     }
 
-    public Avaliacao(int pontuacao, String comentario, String nomeUsuario) {
-        this.pontuacao = pontuacao;
-        this.comentario = comentario;
-        this.nomeUsuario = nomeUsuario;
-    }
 
-    public int getPontuacao() {
-        return pontuacao;
-    }
-
-    public void setPontuacao(int pontuacao) {
-        this.pontuacao = pontuacao;
-    }
-
-    public String getComentario() {
-        return comentario;
-    }
-
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
-
-    public String getNomeUsuario() {
-        return nomeUsuario;
-    }
-
-    public void setNomeUsuario(String nomeUsuario) {
-        this.nomeUsuario = nomeUsuario;
-    }
-
-    @Override
-    public String toString() {
-        return "Avaliacao{" +
-                "pontuacao=" + pontuacao +
-                ", comentario='" + comentario + '\'' +
-                ", nomeUsuario='" + nomeUsuario + '\'' +
-                '}';
-    }
-}
