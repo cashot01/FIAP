0011ae8dfe4b1e20c841fa075c08486c2fffd30d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-05-24 11:48:39 -0300

    atividade4

--- python/aulaEntradaSaidaProcDados/subalgoritimos/atividade4.py
+++ python/aulaEntradaSaidaProcDados/subalgoritimos/atividade4.py
@@ -1,6 +1,47 @@
-# Cauan Passos RM555466
+# Cauan Passos RM 555466
 import os
 os.system("cls")
+intervalo = list()
+contador1 = 0
+contador2 = 0
+contador3 = 0
+
+
+def intervalo():
+    print("INTERVALO")
+    n1 = int(input("1º numero: "))
+    n2 = int(input("2º numero: "))
+    
+    if n1 < n2:
+        menor = n1
+        maior = n2
+    else:
+        menor = n2
+        maior = n1
+    
+    for i in range(menor, maior + 1, 1 ):
+        intervalo = list()
+        intervalo.append(i)
+        print(f"{intervalo}", end = "")
+
+def intervalo_aberto_fechado():
+    print("INTERVALO -Aberto e Fechado")
+    n1 = int(input("1º numero: "))
+    n2 = int(input("2º numero: "))
+    
+    if n1 < n2:
+        menor = n1
+        maior = n2
+    else:
+        menor = n2
+        maior = n1
+    
+    for i in range(menor, maior + 1, 1 ):
+        intervalo = list()
+        intervalo.append(i)
+        print(f"{intervalo}", end = "")
+
+
 def menu():
     print("""
     MENU
@@ -23,12 +64,13 @@ def menu():
         0 -SAIR""")
 
         escolha = int(input("Escolha: "))
-        match escolha:
-            case 1:
-                print("INTERVALO")
-                n1 = int(input("1º numero: "))
-                n2 = int(input("1º numero: "))
-                
+    match escolha:
+        case 1:
+            intervalo()
+            
+        case 2:
+            intervalo_aberto_fechado()
+
                 
         
 
@@ -41,5 +83,5 @@ def menu():
 
 
 
-#---------------------------------------------------------- Programa
+#---------------------------------------------------------- Programa Principal
 menu()
