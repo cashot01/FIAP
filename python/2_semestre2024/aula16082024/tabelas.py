9a2cc0a4d144ef528bea4de0b77325a9293fcd7e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-08-16 10:55:07 -0300

    tabelas python

---
+++ python/2_semestre2024/aula16082024/tabelas.py
@@ -0,0 +1,36 @@
+import os
+os.system("cls")
+
+def lista_tabela(t: list) -> None:
+    for i in range(0, len(tabela), 1):
+        print(f"Registro: {i+1}")
+        print(f"Nome........: {t[i]['nome']}")
+        print(f"Idade........: {t[i]['idade']}")
+        print()
+
+
+tabela = list()
+agenda = {
+    'nome': 'Edson',
+    'idade': 50,
+}
+
+tabela.append(agenda.copy())
+
+agenda['nome'] = input("Nome: ")
+agenda['idade'] = input("Idade: ")
+tabela.append(agenda.copy())
+
+agenda['nome'] = input("Nome: ")
+agenda['idade'] = input("Idade: ")
+tabela.append(agenda.copy())
+
+# for i in range(0, len(tabela), 1):
+#     print(f"Registro: {i+1}")
+#     print(f"Nome........: {tabela[i]['nome']}")
+#     print(f"Idade........: {tabela[i]['idade']}")
+#     print()
+# esse for foi jogado na função listar_tabela
+
+lista_tabela(tabela)
+
