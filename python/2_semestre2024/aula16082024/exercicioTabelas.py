21778a1bc5eda939457da06809d7ac86a0a22aaf
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-08-16 11:56:35 -0300

    exercicio tabela python

---
+++ python/2_semestre2024/aula16082024/exercicioTabelas.py
@@ -0,0 +1,93 @@
+import os
+os.system("cls")
+"""
+criar dicionario com 5 chaves, 1ª chave unica(cpf)
+menu:
+0 - sair
+1 consultar
+2 listar registros
+3 cadastrar
+4 editar
+5 excluir
+
+"""
+
+def exibe_menu() -> None:
+    print(""" 
+    0 - sair
+    1 - consultar
+    2 - listar registros
+    3 - cadastrar
+    4 - editar
+    5 - excluir
+ """)
+
+def consultar() -> None:
+    print("consulta\n")
+    cpf = input("cpf: ")
+    if resgistro.get(cpf):
+        nome = resgistro[cpf]
+        print(f"CPF: {cpf} nome: {nome} ")
+
+def listar_registros(t: list) -> None:
+    os.system("cls")
+    for i in range(0, len(tabela), 1):
+        print(f"Registro: {i+1}")
+        print(f"CPF.............: {t[i]['cpf']}")
+        print(f"Nome.............: {t[i]['nome']}")
+        print(f"Idade.............: {t[i]['idade']}")
+        print(f"Cidade.............: {t[i]['cidade']}")
+        print(f"Faculdade.............: {t[i]['faculdade']} \n")
+        # input(msg_continuar)
+
+def cadastrar() -> None:
+    os.system("cls")
+    resgistro['cpf'] = input("CPF: ")
+    resgistro['nome'] = input("nome: ")
+    resgistro['idade'] = input("idade: ")
+    resgistro['cidade'] = input("cidade: ")
+    resgistro['faculdade'] = input("faculdade: ")
+    tabela.append(resgistro.copy())
+    input(msg_continuar)
+
+
+
+
+#============================= Principal
+msg_continuar = "\nPressione alguma tecla para continuar . . ."
+
+tabela = list()
+resgistro = {
+    'cpf': 1,
+    'nome': 'cauan',
+    'idade' : 18 ,
+    'cidade': 'osasco',
+    'faculdade' : 'fiap'
+}
+
+tabela.append(resgistro.copy())
+
+while True:
+    
+
+    exibe_menu()
+    escolha = input("Escolha: ")
+    match escolha:
+        case "0":
+            break
+        case "1":
+            consultar()
+        case "2":
+            listar_registros(tabela)
+        case "3":
+            cadastrar()
+        case "4":
+            editar()
+        case "5":
+            excluir()
+        case _:
+            input(msg_continuar)
+            os.system("cls")
+            exibe_menu()
+
+
