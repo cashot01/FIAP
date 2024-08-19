e0b709c48d391e32e979645da6505ad4d4f9e128
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-08-19 09:51:37 -0300

    exercicio tabela

--- python/2_semestre2024/aula16082024/exercicioTabelas.py
+++ python/2_semestre2024/aula16082024/exercicioTabelas.py
@@ -1,126 +1,132 @@
-import os
-os.system("cls")
-"""
-criar dicionario com 5 chaves, 1ª chave unica(cpf)
-menu:
-0 - sair
-1 consultar
-2 listar registros
-3 cadastrar
-4 editar
-5 excluir
+func = {
+    'cpf' : 123,
+    'nome' : 'Edson de Oliveira',
+    'salario': 23456.78,
+}
+tabela = []
 
-"""
+tabela.append(func.copy())
 
-def exibe_menu() -> None:
-    print(""" 
-    0 - sair
-    1 - consultar
-    2 - listar registros
-    3 - cadastrar
-    4 - editar
-    5 - excluir
- """)
-    
-def voltar_menu(msg: str) -> None:
-    msg = input(msg)
-    os.system("cls")
+def exibe_menu():
+    print("""
+    0 - SAIR
+    1 - Cadastrar Funcionário
+    2 - Consultar Funcionário
+    3 - Editar Funcionário
+    4 - Excluir Funcionário
+    5 - Listar Funcionários
+    """)
 
-"""
-retorna o numero do indice se encontrado, caso contrario retorna -1
-t: Tabela -> list
-
-"""
+# Retorna o numero do índice se encontrado, caso contrário retorna -1
+# t: Tabela -> list
+# cpf: id de busca -> int
+# Retorno: int [índice encontrado]
 def busca_cpf(t: list, cpf: int) -> int:
-    for i in range(0, len(t), 1):
+    for i in range(0,len(t),1):
         if t[i]['cpf'] == cpf:
             return i
     return -1
 
-
-def consultar():
-    os.system("cls")
-    print("consulta\n")
-    cpf = input("cpf: ")
-
-    if resgistro.get(cpf):
-        nome = resgistro[nome]
-        print(f"CPF existe ")
-    else:
-        print(f"O CPF {cpf} não existe")
-
-    voltar_menu(msg_continuar)
-
-def exibe_registro(t, indice):
-    print(f"\nCPF....: {t[indice]['cpf']}")
+def exibe_funcionario(t, indice) -> None:
+    print(f"\nCPF......: {t[indice]['cpf']}")
     print(f"Nome.....: {t[indice]['nome']}")
-    print(f"Idade....: {t[indice]['idade']}")
-    print(f"Cidade...: {t[indice]['cidade']}")
-    print(f"Faculdade: {t[indice]['faculdade']}")
+    print(f"Salário..: {t[indice]['salario']}")
 
-def listar_registros(t: list) -> None:
-    os.system("cls")
-    for i in range(0, len(tabela), 1):
-        print(f"Registro: {i+1}")
-        print(f"CPF..................: {t[i]['cpf']}")
-        print(f"Nome.................: {t[i]['nome']}")
-        print(f"Idade................: {t[i]['idade']}")
-        print(f"Cidade...............: {t[i]['cidade']}")
-        print(f"Faculdade............: {t[i]['faculdade']} \n")
-    voltar_menu(msg_continuar)
+def listar_funcionarios(t: list) -> None:
+    mensagem_titulo("CPF        NOME                SALARIO")
+    for i in range(len(t)):
+        print(f"{t[i]['cpf']} | {t[i]['nome']} | {t[i]['salario']}")
 
-def editar(t: list, i: int, id: int) -> None:
-    exibe_registro(t, i)
+def edita_funcionario(t: list, i: int, id: int) -> None:
+    exibe_funcionario(t, i)
     t[retorno]['cpf'] = id
+    t[retorno]['nome'] = input("Nome....:")
+    t[retorno]['salario'] = float(input('Salário.:'))
+    mensagem("!" ,"Editado com sucesso!")
 
-def cadastrar() -> None:
-    os.system("cls")
-    resgistro['cpf'] = input("CPF: ")
-    resgistro['nome'] = input("nome: ")
-    resgistro['idade'] = input("idade: ")
-    resgistro['cidade'] = input("cidade: ")
-    resgistro['faculdade'] = input("faculdade: ")
-    tabela.append(resgistro.copy())
-    voltar_menu(msg_continuar)
-
-
+def cadastra_funcionario(t: list, d: dict, id: int) -> None:
+    d['cpf'] = id
+    d['nome'] = input("Nome....: ")
+    d['salario'] = float(input('Salário.: '))
+    t.append(d.copy())
+    mensagem("!" ,"Cadastrado com sucesso!")
 
 
-#============================= Principal
-msg_continuar = "\nPressione alguma tecla para continuar . . ."
+def exclui_funcionario(l: list, id: int) -> None:
+    resp = input("Confirma a exclusão do funcionário?")
+    if resp.upper() == 'S':
+        l.pop(id)
+        mensagem("-" ,"Funcionário excluído!")
+    else:
+        print("Exclusão cancelada . . .")
 
-tabela = list()
-resgistro = {
-    'cpf': 1,
-    'nome': 'cauan',
-    'idade' : 18 ,
-    'cidade': 'osasco',
-    'faculdade' : 'fiap'
-}
+# Exibe uma mensagem formatada.
+# Parâmetro 1: carac -> str: O caractere que formará a mensagem
+# Parâmetro 2: msg -> str: A mensagem que será exibida
+# Retorno: None
+def mensagem(carac:str, msg: str) -> None:
+    tamanho = len(msg)
+    print(f"\n{carac * tamanho}")
+    print(msg)
+    print(f"{carac * tamanho}")
 
-tabela.append(resgistro.copy())
+# Formata a mensagem de título.
+# Parâmetro: msg -> str: A mensagem que será exibida no título
+# Retorno: None
+def mensagem_titulo(msg: str) -> None:
+    tamanho = len(msg)
+    print(f"\n{msg}\n{tamanho * '='}")
 
+
+import os
 while True:
-    
-
+    os.system("clear")
+    mensagem_titulo("M E N U")
     exibe_menu()
     escolha = input("Escolha: ")
+    os.system("clear")
     match escolha:
-        case "0":
+        case '0':
             break
-        case "1":
-            consultar()
-        case "2":
-            listar_registros(tabela)
-        case "3":
-            cadastrar()
-        case "4":
-            editar()
-        case "5":
-            excluir()
+        case '1':
+            mensagem_titulo("CADASTRANDO FUNCIONÁRIO:")
+            cpf = int(input("\nCPF.....: "))
+            retorno = busca_cpf(tabela, cpf)
+            if retorno == -1:
+                cadastra_funcionario(tabela, func, cpf)
+            else:
+                mensagem("-" ,"Funcionário já existe!")
+                
+        case '2':
+            mensagem_titulo("CONSULTANDO FUNCIONARIO")
+            cpf = int(input("\nCPF.....: "))
+            retorno = busca_cpf(tabela, cpf)
+            if  retorno != -1:
+                exibe_funcionario(tabela, retorno)
+            else:
+                mensagem("-" ,"Funcionário inexistente!")
+        case '3':
+            mensagem_titulo("EDITANDO FUNCIONARIO")
+            cpf = int(input("\nCPF.....: "))
+            retorno = busca_cpf(tabela, cpf)
+            if  retorno != -1:
+                edita_funcionario(tabela, retorno, cpf)
+            else:
+                mensagem("-" ,"Funcionário inexistente!")
+        case '4':
+            mensagem_titulo("EXCLUINDO FUNCIONARIO")
+            cpf = int(input("CPF.....: "))
+            retorno = busca_cpf(tabela, cpf)
+            if  retorno != -1:
+                exibe_funcionario(tabela, retorno)
+                exclui_funcionario(tabela, retorno)
+                
+            else:
+                mensagem("-" ,"Funcionário inexistente!")
+        case '5':
+            listar_funcionarios(tabela)
         case _:
-            input(msg_continuar)
-            os.system("cls")
-            exibe_menu()
-
+            mensagem("-" ,"Opção inexistente!")
+            
+    input("\nPressione alguma tecla para continuar...")
 
