4d627eca5e6a73b3517bb28785c7d7443b33861c
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-08-21 10:47:49 -0300

    log4j

--- java/IniciandoComLog4j/src/main/java/fiap/logs/Main.java
+++ java/IniciandoComLog4j/src/main/java/fiap/logs/Main.java
@@ -1,29 +1,37 @@
-package fiap.logs;
-
+// importar biblioteca do log4j
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class Main {
     public static Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        //var logger = LogManager.getLogManager();
+        //logger.getLogger("").info("teste");
+
         try{
+            var conteudo = "Esse é o conteúdo que será exportado para o arquivo txt";
+            var arquivo = new File("arquivo.txt");
 
-            logger.info("Sistema iniciando...");
-            var scanner = new Scanner(System.in);
-            System.out.println("Digite um nº:");
-            int numero = scanner.nextInt();
+            // Criar o Processo de Escrita no Arquivo
 
+            try(var writer = new FileWriter(arquivo)){
+                writer.write(conteudo);
+                writer.flush();
+            }
+            catch (IOException e){
+                logger.error("Erro ao escrever no arquivo: " + e.getMessage());
+            }
         }
         catch (Exception e){
-            e.printStackTrace();
-            logger.fatal("Erro fatal: " + e.getMessage() + " - " +
-                    Arrays.toString(e.getStackTrace()));
+            // problema muito grave que irá fechar o sistema
+            logger.fatal("Erro fatal: " + e.getMessage() + " - " + Arrays.toString(e.getStackTrace()));
 
         }
-
     }
 }
