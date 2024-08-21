e1fcd0f6ec198c38baab1a8d3363363391e4fd4a
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-08-21 11:04:19 -0300

    log4j

--- java/IniciandoComLog4j/src/main/java/Main.java
+++ java/IniciandoComLog4j/src/main/java/Main.java
@@ -2,35 +2,40 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
     public static Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //var logger = LogManager.getLogManager();
         //logger.getLogger("").info("teste");
 
-        try{
+        File arquivo = null;
+        try {
             var conteudo = "Esse é o conteúdo que será exportado para o arquivo txt";
-            var arquivo = new File("arquivo.txt");
+            arquivo = new File("arquivo.txt");
 
             // Criar o Processo de Escrita no Arquivo
 
-            try(var writer = new FileWriter(arquivo)){
-                writer.write(conteudo);
-                writer.flush();
-            }
-            catch (IOException e){
+            try (var writer = new FileWriter(arquivo)) {
+                //writer.write(conteudo);
+                //writer.flush();
+            } catch (IOException e) {
                 logger.error("Erro ao escrever no arquivo: " + e.getMessage());
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             // problema muito grave que irá fechar o sistema
             logger.fatal("Erro fatal: " + e.getMessage() + " - " + Arrays.toString(e.getStackTrace()));
+
+        }
+
+        // ler o arquivo
+        try (var reader = new BufferedReader(new FileReader(arquivo))) {
+            String linha;
+            while (reader.readLine() != null) {
+                System.out.println(linha);
+            }
 
         }
     }
