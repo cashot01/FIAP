6c90f6fa7489427e089ce31ae0e99a5d202bc49d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-08-22 11:21:51 -0300

    FIAP_banco sql

---
+++ banco/FIAP_banco.sql
@@ -0,0 +1,22 @@
+
+-- todos os objetos criados precisam tem um ; no final
+
+/*
+- Banco de dados tem:
+    - USUARIOS:
+        - TABELAS, VIEWS, SEQUENCES, ETC
+DENTRO DO BANCO NÃO PODE TER USUARIOS REPETIDOS 
+    DENTRO DO USUARIO NÃO PODE TER OBJETOS REPETIDOS
+        DENTRO DA TABELA NÃO PODE TER COLUNAS REPETIDAS (OU DUPLICADAS)
+
+*/
+
+
+create table Aluno(
+    id_aluno int PRIMARY KEY,
+    nm_aluno VARCHAR(60) not null,
+    dt_nascimento date,
+    cpf varchar(15),
+    rg varchar(15), 
+    CONSTRAINT uk_rg_cpf UNIQUE(cpf, rg) -- chave unica composta 
+);
