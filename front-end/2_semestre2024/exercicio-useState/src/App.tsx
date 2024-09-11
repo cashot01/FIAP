288123cfc3c4d194a0b92e8f24ad18e2ffe92ae3
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-11 11:08:58 -0300

    exercicio useState

--- front-end/2_semestre2024/exercicio-useState/src/App.tsx
+++ front-end/2_semestre2024/exercicio-useState/src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [count, setCount] = useState<number>(0)
+
+  const aumentar = ()=>{
+    setCount(count + 1)
+  }
+
+  const diminuir = ()=>{
+    setCount(count - 1)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Exercicio UseSate</h1>
+     <h2>Contador</h2>
+     <p> {count}</p>
+     <button onClick={aumentar}>Aumentar</button>
+     <button onClick={diminuir}>Diminuir</button>
     </>
   )
 }
