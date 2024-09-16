d6cd280e72b0a644b7839f5501f5706f472ff2e2
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-16 09:34:38 -0300

    ativ2_cp5

---
+++ python/2_semestre2024/aula16092024/atv2_cp5.py
@@ -0,0 +1,67 @@
+# definição subalgoritimos
+def cadastrar_registro(id: str, registros: dict) -> None:
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    registros[id] = {
+        'cpf': id, 
+        'nome': nome, 
+        'idade': idade
+    }
+
+    """
+    '123' = {
+        'cpf': '123',
+        'nome': 'edson',
+        'idade': '40',
+    }
+
+    '234' = {
+        'cpf': '234',
+        'nome': 'maria',
+        'idade': '34',
+    }
+
+    """
+
+    print("Cadastrado com sucesso")
+
+def exibe_registro(registro: str) -> None:
+    print(f"\nCPF: {registro['cpf']}")
+    print(f"Nome: {registro['nome']}")
+    print(f"Idade: {registro['idade']}\n")
+
+
+def editar_registro(id: str, registros: dict) -> None:
+    # apresentando registro existente
+    exibe_registro(registros[id])
+
+    nome = input("Nome: ")
+    idade = input("Idade: ")
+    # editando registro encontrado
+    if nome != "":
+        registros[id]['nome'] = nome
+    if idade != "":
+        registros[id]['idade'] = idade
+    print("Editado com sucesso")
+
+
+
+
+# programa principal
+import os
+os.system("cls")
+
+pessoas = {}
+
+continua = True
+while continua:
+    cpf = input("CPF: ")
+    if cpf == "":
+        continua = False
+    elif cpf in pessoas:
+        editar_registro(cpf, pessoas)
+    else:
+        cadastrar_registro(cpf, pessoas)
+else:
+    print("Programa finalizado")
+        
