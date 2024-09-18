432b95c132eb5cba787742697c3195eb922707ba
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 10:46:44 -0300

    artista

--- java/exercicioArtistaMusica/src/main/java/entidade/Artista.java
+++ java/exercicioArtistaMusica/src/main/java/entidade/Artista.java
@@ -4,5 +4,36 @@ public class Artista extends _EntidadeBase{
     private String nome;
     private String generoMusial;
 
+    public Artista() {
+    }
 
+    public Artista(int id, String nome, String generoMusial) {
+        super(id);
+        this.nome = nome;
+        this.generoMusial = generoMusial;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGeneroMusial() {
+        return generoMusial;
+    }
+
+    public void setGeneroMusial(String generoMusial) {
+        this.generoMusial = generoMusial;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" +
+                "nome='" + nome + '\'' +
+                ", generoMusial='" + generoMusial + '\'' +
+                "} " + super.toString();
+    }
 }
