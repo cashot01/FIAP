81a551f6f324f9b2a97f6f00350ba7e3c14d66ee
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 16:33:24 -0300

    java

--- java/exercicioArtistaMusica/src/main/java/org/example/Main.java
+++ java/exercicioArtistaMusica/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import entidade.Artista;
+import repositorio.ArtistaRepositorio;
+
 public class Main {
     public static void main(String[] args) {
+        var artistaRepositorio = new ArtistaRepositorio();
 
+        var artistas = artistaRepositorio.GetAll();
+
+        var artista = artistaRepositorio.GetById(1);
+
+        artistas.forEach(System.out::println);
+
+        artistaRepositorio.Update(new Artista(1, "Cauan", "Rock"),1);
     }
 }
