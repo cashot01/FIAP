8ef8d315c4de43d7a62cd68e66d7ccd43f1b3cbf
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 11:01:30 -0300

    musica repositorio

--- java/exercicioArtistaMusica/src/main/java/repositorio/MusicaRepositorio.java
+++ java/exercicioArtistaMusica/src/main/java/repositorio/MusicaRepositorio.java
@@ -2,6 +2,34 @@ package repositorio;
 
 import entidade.Musica;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MusicaRepositorio implements _RepositorioBase<Musica>{
 
+
+    @Override
+    public void Insert(Musica entity) {
+        
+    }
+
+    @Override
+    public void Update(Musica entity, int id) {
+
+    }
+
+    @Override
+    public void Delete(int id) {
+
+    }
+
+    @Override
+    public Optional<Musica> GetById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Musica> GetAll() {
+        return List.of();
+    }
 }
