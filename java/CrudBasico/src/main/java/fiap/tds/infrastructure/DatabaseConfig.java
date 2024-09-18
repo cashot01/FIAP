2ec497365f59525bb6f0b5c279c9747f9cef42bb
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 10:26:00 -0300

    crud basico

--- java/CrudBasico/src/main/java/fiap/tds/infrastructure/DatabaseConfig.java
+++ java/CrudBasico/src/main/java/fiap/tds/infrastructure/DatabaseConfig.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     private static String URL = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    private static String USER = "pf1779";
-    private static String PASSWORD = "fiap24";
+    private static String USER = "RM555466";
+    private static String PASSWORD = "071105";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
