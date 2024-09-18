aae43cf6aa9b2928b1fd935d65fbce609e650823
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 10:47:33 -0300

    musica

--- java/exercicioArtistaMusica/src/main/java/entidade/Musica.java
+++ java/exercicioArtistaMusica/src/main/java/entidade/Musica.java
@@ -7,4 +7,46 @@ public class Musica extends _EntidadeBase {
     private  double duracao;
     private Date dataLancamento;
 
+    public Musica() {
+    }
+
+    public Musica(int id, String nomeMusica, double duracao, Date dataLancamento) {
+        super(id);
+        this.nomeMusica = nomeMusica;
+        this.duracao = duracao;
+        this.dataLancamento = dataLancamento;
+    }
+
+    public String getNomeMusica() {
+        return nomeMusica;
+    }
+
+    public void setNomeMusica(String nomeMusica) {
+        this.nomeMusica = nomeMusica;
+    }
+
+    public double getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(double duracao) {
+        this.duracao = duracao;
+    }
+
+    public Date getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(Date dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Musica{" +
+                "nomeMusica='" + nomeMusica + '\'' +
+                ", duracao=" + duracao +
+                ", dataLancamento=" + dataLancamento +
+                "} " + super.toString();
+    }
 }
