f6edd022b2027794e9d45c0032282503daf45c93
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 08:54:36 -0300

    react- useEffect

--- front-end/2_semestre2024/react-useeffect/src/App.tsx
+++ front-end/2_semestre2024/react-useeffect/src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
 import Contagem from "./components/Contagem"
 
 
 function App() {
 
+  const [count, setCount] = useState(0)
+  const [contagem, setContagem] = useState(true)
+
+  function aumentar(){
+    setCount(count + 1)
+  }
+
   return (
     <>
       <h1>React - Hooks - useEffect</h1>
-      <Contagem/>
+      <button onClick={()=>setContagem(!contagem)}>Criar contagem</button>
+      {contagem ? <Contagem count={count} aumentar={aumentar}/> : '' }
+      
     </>
   )
 }
