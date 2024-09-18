9a547e7c0504a1fb2d7fec60b427e50b85d98d25
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-18 08:04:29 -0300

    react effect

---
+++ front-end/2_semestre2024/react-useeffect/src/App.tsx
@@ -0,0 +1,14 @@
+import Contagem from "./components/Contagem"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>React - Hooks - useEffect</h1>
+      <Contagem/>
+    </>
+  )
+}
+
+export default App
---
+++ front-end/2_semestre2024/react-useeffect/src/components/Contagem.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Contagem(){
+
+    return(
+        <div>
+            <h2>Contagem</h2>
+        </div>
+    )
+}
