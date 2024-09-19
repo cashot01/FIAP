c985d165577394c8b09387ca1fd684cbac14424d
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-19 11:19:21 -0300

    aula 6 sql

--- banco/aula06sql.sql
+++ banco/aula06sql.sql
@@ -17,5 +17,20 @@ UPDATE T_ALUNO SET fk_cep=0131415 WHERE RA=1;
 -- UPDATE NÃO PROCEDUAL (GENERICO) PARA N LINHAS
 -- Where é uma restrição de atualização 
 -- quando não há o where, essa restrição n existe
-UPDATE t_aluno set nome='jose'
+UPDATE t_aluno set nm_aluno='jose'
 
+-- APAGAR DADOS DE UMA TABELA
+DELETE FROM T_ALUNO;
+
+
+
+-- DTL (DATA TRANSACTION LANGUAGE)
+-- commit: confirmar transação 
+-- rollback: defazer transação
+-- É utilizada em conjunto com a DLM (INERT, UPDATE, DELETE)
+
+commit;
+
+rollback;
+
+DELETE FROM T_ENDERECO;
