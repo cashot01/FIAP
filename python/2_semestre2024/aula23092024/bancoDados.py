3cf7283de737e3ca750a535891adabed67129420
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-23 10:18:35 -0300

    banco dados python

--- python/2_semestre2024/aula23092024/bancoDados.py
+++ python/2_semestre2024/aula23092024/bancoDados.py
@@ -2,10 +2,11 @@ import os
 import oracledb
 import pandas as pd
 
-# conectando banco dados
+# conectando o banco de dados
 try:
-    # efetua a conexão com usuario
-    conn = oracledb.connect(user="RM555466", password="071105", dsn="oracle.fiap.com.br:1521/ORCL")
+    # efetua a conexao com o usuário
+    conn = oracledb.connect(user="RM555466", password = "071105", dsn='oracle.fiap.com.br:1521/ORCL')
+
 
     # criar objetos para cada operação no banco dados
     inst_cadastro = conn.cursor()
@@ -19,47 +20,42 @@ except Exception as e:
 else:
     conexao = True
 
-# enquanto o flag estiver apontando para conexão
+# Enquanto o flag estiver apontando para conexao
 margem = ' ' * 4
 while conexao:
-    os.system("cls")
-    # apresenta menu
+    # os.system("cls")
+ 
+    # Apresenta o menu
     print("""
     CRUD - PETSHOP
     1 - Cadastrar Pet
-    2 - Listar Pet
+    2 - Listar Pets
     3 - Alterar Pet
     4 - Excluir Pet
-    5 - Excluir todos pets
+    5 - Excluir todos os Pets
     6 - SAIR
-""")
+    """)
     escolha = int(input(margem + "Escolha -> "))
     os.system("cls")
     match escolha:
         case 1:
             try:
-                print("------ Cadastrar Pet ------ ")
-                
-                # recebe os valores para o cadastro
-                tipo = input(margem + "Tipo......: ")
-                nome = input(margem + "Nome......: ")
-                idade = int(input(margem + "Idade......: "))
-                cadastro = f"""INSERT INTO petshop(tipo_pet, nome_pet, idade) values ({tipo}, {nome}, {idade}) """
-
-                # executa a gravação do registro na tabela
+                print("------ CADASTRAR PET ------")
+                # Recebe os valores para o cadastro
+                tipo = input(margem + "Tipo.....: ")
+                nome = input(margem + "Nome.....: ")
+                idade = int(input(margem + "Idade....: "))
+                cadastro = f"""INSERT INTO petshop(tipo_pet, nome_pet,idade) values ({tipo},{nome},{idade})"""
+                # Executa a gravação do registro na tabela
                 inst_cadastro.execute(cadastro)
                 conn.commit()
-            
             except ValueError:
                 print(margem + "Digite um numero na idade")
             except:
-                print(margem + "Erro alguma coisa")
+                print(margem + "Erro em alguma coisa")
             else:
-                print(margem + "Dados gravados com sucesso")
-                
+                print(margem + "DADOS GRAVADOS COM SUCESSO!")
         case _:
-            print(margem + "Opção invalida")
-
-
+            print(margem + "Opcao invalida!")
 else:
-    print("Obrigado por utilizar nossa aplicação :)")
+    print("Obrigado por utilizar a nossa aplicação :)")
