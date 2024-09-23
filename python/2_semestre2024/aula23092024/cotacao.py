e16013dd7f224eef0ecae20a9ded2f783a727669
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-23 08:47:42 -0300

    euro

--- python/2_semestre2024/aula23092024/cotacao.py
+++ python/2_semestre2024/aula23092024/cotacao.py
@@ -5,7 +5,7 @@ def pegar_cotacoes():
     requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
     requisicao_dic = requisicao.json()
     cotacao_dolar = requisicao_dic['USDBRL']['bid']
-    cotacao_euro = requisicao_dic['EUROBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
     cotacao_btc = requisicao_dic['BTCBRL']['bid']
     texto_resposta['text'] = f'''
     Dólar: {cotacao_dolar}
