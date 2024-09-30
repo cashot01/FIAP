bd2c45327745eb3ac6bfa85d68f484a3213e7fc0
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-09-30 09:17:21 -0300

    cp5 python

--- python/2_semestre2024/aula30092024/cp5_correcao.py
+++ python/2_semestre2024/aula30092024/cp5_correcao.py
@@ -3,4 +3,45 @@ os.system("cls")
 
 #--------- Verificar se um login é valido
 def login_valido(l: str) -> bool:
-    
+    digito = "01234567890"
+    if len(l) != 6:
+        return False
+    else:
+        if l[0] not in digito:
+            return False
+        else:
+            letra = "ABCDEFGHIJKLMNOPQRSTUVXWYZ"
+            achou = False
+            for i in range(1, 6, 1):
+                if l[i].upper() in letra:
+                    achou = True
+                    break
+            return achou
+        
+
+
+# verificar senha valida
+def senha_valida(s: str) -> bool:
+    if len(s) < 6:
+        return False
+    else:
+        letra = "ABCDEFGHIJKLMNOPQRSTUVXWYZ"
+        if s[0].upper() not in letra:
+            return False
+        else:
+            digito = "01234567890"
+            achou = False
+            for i in range(1, len(s), 1):
+                if s[i] in digito:
+                    achou = True
+                    break
+            return achou
+        
+
+# -------- Processa o arquivo dependendo do modo de abertura
+def processa_arquivo(nome_arquivo: str, modo_abertura: str) -> None:
+    match modo_abertura:
+        case "a":
+            # Gravação do login e senha validados no arquivo
+            with open(nome_arquivo, modo_abertura, encoding="UTF-8") as arq:
+                arq.write(f"{login}, {senha}")
