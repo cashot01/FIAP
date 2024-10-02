c61e7ccb28be8726ec982674361616515a738a7b
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-02 09:15:41 -0300

    route id

--- front-end/2_semestre2024/next-crud/src/app/api/base-produtos/[id]/route.ts
+++ front-end/2_semestre2024/next-crud/src/app/api/base-produtos/[id]/route.ts
@@ -1,7 +1,16 @@
+import { TipoProduto } from "@/types";
+import { promises as fs } from "fs";
+import { NextResponse } from "next/server";
 
 
-export async function GET(){
+export async function GET(request:Request, {params}:{params:{id:number}}){
 
-    console.log("Pediram o ID 1 ?");
+    const file = await fs.readFile(process.cwd() + '/src/data/base.json', 'utf-8')
+
+    const produtos:TipoProduto[] = JSON.parse(file)
+
+    const produto = produtos.find(p => p.id == params.id)
+
+    return NextResponse.json(produto);
     
 }
---
+++ front-end/2_semestre2024/next-crud/src/types.ts
@@ -0,0 +1,8 @@
+
+
+export type TipoProduto ={
+    id:number;
+    nome:string;
+    preco:number;
+    estoque:number;
+}
