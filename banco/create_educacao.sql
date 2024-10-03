24949546e40b4f4fbf4671132dade9d733042665
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-03 11:07:32 -0300

    sql 9 funções

--- banco/create_educacao.sql
+++ banco/create_educacao.sql
@@ -15,4 +15,49 @@ CREATE TABLE Dados_Escolas (
     projecao NUMBER
 );
 
+-- DROP TABLE Dados_Escolas;
 
+-- CONSULTA SIMPLES
+SELECT * FROM Dados_Escolas;
+
+-- funções numericas
+SELECT nota_saeb_media_padronizada FROM Dados_Escolas;
+
+SELECT 
+        nota_saeb_media_padronizada, 
+        ROUND(nota_saeb_media_padronizada, 2) round_nota, 
+        TRUNC (nota_saeb_media_padronizada, 0) trunc_nota,
+        MOD (nota_saeb_media_padronizada, 2) mod_nota,
+        (nota_saeb_media_padronizada * (-1)) vlr_negativo,
+        ABS(nota_saeb_media_padronizada * (-1)) vlr_abs,
+        SQRT (nota_saeb_media_padronizada) raiz_quadrada,
+        CEIL (nota_saeb_media_padronizada) arr_up,
+        FLOOR (nota_saeb_media_padronizada) arr_down
+
+FROM Dados_Escolas;
+
+
+-- FUNÇÕES DE TEXTO
+SELECT 
+    rede,
+    UPPER(rede) maiusc,
+    LOWER(rede) minusc,
+    INITCAP(rede) mai_min
+FROM Dados_Escolas;  
+
+
+
+SELECT 
+    rede,
+    CONCAT('escola ', rede),
+    SUBSTR(rede, 3, 4),
+    LENGTH(rede),
+    LPAD(rede, 10, '.'),
+    RPAD(rede, 10, '.'),
+    LTRIM(rede, 'to'),
+    RTRIM(rede, 'tal'),
+    REPLACE(rede, 'to', 'TO')
+FROM Dados_Escolas;    
+
+-- APELIDOS
+-- PODE SER DEFENIDOS COM A CLAUSULA AS (ALIAS) OU A FRENTE DO OBJETO
