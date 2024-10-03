49b22a1e1f7f3043b600453291c33a65c1137fb8
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-03 10:29:11 -0300

    sql aula 9

---
+++ banco/create_educacao.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Dados_Escolas (
+    ano INT,
+    sigla_uf VARCHAR(2),
+    id_municipio VARCHAR(10),
+    id_escola VARCHAR(10),
+    rede VARCHAR(10),
+    ensino VARCHAR(15),
+    anos_escolares VARCHAR(20),
+    taxa_aprovacao NUMBER,
+    indicador_rendimento DECIMAL(10, 8),
+    nota_saeb_matematica NUMBER,
+    nota_saeb_lingua_portuguesa NUMBER,
+    nota_saeb_media_padronizada NUMBER,
+    ideb NUMBER,
+    projecao NUMBER
+);
