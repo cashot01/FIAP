90ee35993dcc53b4d7d8cb0a8dfee0888d31b34a
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-08 09:39:49 -0300

    next crud 2

---
+++ front-end/2_semestre2024/next-crud-2/src/app/produtos/produto/[id]/page.tsx
@@ -0,0 +1,16 @@
+import { TipoProduto } from "@/types"
+import { useState } from "react"
+
+
+export default function Produto({params}:{params:{id:number}}){
+
+    const [produto, setProduto] = useState<TipoProduto>()
+
+
+
+
+
+    return(
+        <main></main>
+    )
+}
