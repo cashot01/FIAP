d22228657e2a84d5053930baa48cfa609849ef8e
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-09 09:39:51 -0300

    next

---
+++ front-end/2_semestre2024/next-crud-2/src/app/produtos/cad-produtos/page.tsx
@@ -0,0 +1,72 @@
+"use client"
+import { TipoProduto } from "@/types";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+
+export default function CadastroProdutos(){
+
+    const navigate = useRouter()
+
+    const [produto, setProduto] = useState<TipoProduto>({
+        id:0,
+        nome:'',
+        preco:0,
+        estoque:0
+    })
+
+    // capta os dados digitatos e insere no objeto produto
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        const {name, value} = e.target
+        setProduto({...produto, [name]:value })
+    }
+
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault()
+
+        const cabecalho = {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(produto)
+        }
+
+        try{
+            const response = await fetch("http://localhost:3000/api/base-produtos", cabecalho)
+
+            if(response.ok){
+                alert(`${produto.nome} cadastrado com sucesso`)
+                setProduto({
+                    id:0,
+                    nome:'',
+                    preco:0,
+                    estoque:0
+                })
+                navigate.push("/produtos")
+            }
+        }
+    }
+
+    return(
+        <main>
+            <h1>Cadastro de Produtos</h1>
+            <p>Aqui inserimos um novo produto assim que chega na loja.</p>
+            <form>
+                <div>
+                    <label htmlFor="idNome">Nome</label>
+                    <input type="text" name="nome" id="idNome" onChange={handleChange} value={produto.nome}/>
+                </div>
+                <div>
+                    <label htmlFor="idPreco">Preço</label>
+                    <input type="number" step={'0.01'} name="preco" id="idPreco" onChange={handleChange} value={produto.preco} />
+                </div>
+                <div>
+                    <label htmlFor="idEstoque">Estoque</label>
+                    <input type="number" name="estoque" id="idEstoque" onChange={handleChange} value={produto.estoque} />
+                </div>
+                <button type="submit">Cadastrar Produto</button>
+            </form>
+        </main>
+    )
+}
