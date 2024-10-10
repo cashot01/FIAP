c9da271d81c59590dec319ef2e3532abfc1ff912
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-10 08:56:55 -0300

    ia

--- chatbotIA/minhaAPI.py
+++ chatbotIA/minhaAPI.py
@@ -1,15 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify  # Corrigido de jsonfy para jsonify
 import numpy as np
 import pickle
-
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Olá mundo"
-
-if __name__ == "__main__":
-    app.run()
-
-with open('C:\Users\labsfiap\Documents\GitHub\FIAP\chatbotIA\pipelineModel1.pkl', 'rb') as f:
+ 
+# Corrigido a forma de abrir o arquivo
+with open('chatbotIA/pipelineModel 2.pkl', 'rb') as f:
     modelo = pickle.load(f)
+ 
+@app.route('/prever', methods=['GET'])
+def prever():
+    parametro1 = float(request.args.get('comp_abd'))
+    parametro2 = float(request.args.get('comp_ant'))
+ 
+    entrada = np.array([[parametro1, parametro2]])
+    resultado = modelo.predict(entrada)
+ 
+    return jsonify({'previsao': resultado.tolist()})
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
