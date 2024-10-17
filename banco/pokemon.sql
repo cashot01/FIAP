6de6310ed6c865896d5fc4a7879800cec5bf955b
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-17 11:29:48 -0300

    self join + inserts

--- banco/pokemon.sql
+++ banco/pokemon.sql
@@ -46,38 +46,38 @@ VALUES (7, 'Missingno', NULL, 38, 41, 40);
 SELECT * FROM Pokemons;
 SELECT * FROM Tipos; 
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (8, 'Psyduck', 2, 50, 52, 48);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (8, 'Psyduck', 2, 50, 52, 48);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (9, 'Oddish', 3, 45, 50, 55);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (9, 'Oddish', 3, 45, 50, 55);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (10, 'Magnemite', 4, 25, 35, 70);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (10, 'Magnemite', 4, 25, 35, 70);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (11, 'Abra', 5, 25, 20, 15);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (11, 'Abra', 5, 25, 20, 15);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (12, 'Pidgey', 6, 40, 45, 40);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (12, 'Pidgey', 6, 40, 45, 40);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (13, 'Growlithe', 1, 55, 70, 45);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (13, 'Growlithe', 1, 55, 70, 45);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (14, 'Horsea', 2, 30, 40, 70);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (14, 'Horsea', 2, 30, 40, 70);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (15, 'Bellsprout', 3, 50, 75, 35);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (15, 'Bellsprout', 3, 50, 75, 35);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (16, 'Voltorb', 4, 40, 30, 50);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (16, 'Voltorb', 4, 40, 30, 50);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (17, 'Drowzee', 5, 60, 48, 45);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (17, 'Drowzee', 5, 60, 48, 45);
 
--- INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
--- VALUES (18, 'Spearow', 6, 40, 60, 30);
+INSERT INTO Pokemons (PokemonID, Nome, TipoID, HP, Ataque, Defesa) 
+VALUES (18, 'Spearow', 6, 40, 60, 30);
 
 -- APELIDOS
 
