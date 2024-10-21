e147bb51fb87247bdb26dca5c4d4d5e8d2b29122
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-21 09:15:26 -0300

    ex importação

--- python/2_semestre2024/aula18102024/ex_importacao.py
+++ python/2_semestre2024/aula18102024/ex_importacao.py
@@ -1,16 +1,28 @@
 import os
 os.system("cls")
 import pandas as pd
+import oracledb
 import datetime
+
+def cadastro():
 
 
 def main():
+    try:
+        conn = oracledb.connect(user="RM555466", password="071105", dsn='oracle.fiap.com.br:1521/ORCL')
+    
+        inst_cadastro = conn.cursor()
+        inst_consulta = conn.cursor()
+    except Exception as e:
+        print("Erro: ", e)
+
     while True:
         os.system("cls")
         print("Menu")
         print("0 - Sair")
         print("1 - Cadastrar")
-        print("2 - Listar / Cadastrar")
+        print("2 - Listar / Exportar")
+        print("3 - Filtrar Registros")
         opcao = input("Escolha: ")
         match opcao:
             case "0":
@@ -19,6 +31,8 @@ def main():
                 cadastrar()
             case "2":
                 listar_exportar()
+            case "3":
+                filtrar_registros()
             case _:
                 print("opção invalida")
                 input("presione enter para continuar...")
