043cc4924d6675ff12c8ddf0a73dec3331dd1501
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-22 08:17:16 -0300

    imagem

--- front-end/2_semestre2024/next-image/src/app/page.tsx
+++ front-end/2_semestre2024/next-image/src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import carro from './../image/carro.jpg'
 
 export default function Home() {
   return (
     <div>
-      
+      <Image src={carro} alt="Bugatti"/>
     </div>
   );
 }
