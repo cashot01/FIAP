f095b58dfee0693012be2f4b683174bfd21f0995
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-22 09:35:59 -0300

    next image

--- front-end/2_semestre2024/next-image/src/app/page.tsx
+++ front-end/2_semestre2024/next-image/src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import Image from 'next/image';
 import carro from './../image/carro.jpg'
 
 export default function Home() {
   return (
     <div>
       <Image src={carro} alt="Bugatti"/>
+      <Image src="/image/carro.jpg" alt="Bugatti 2" width={1024} height={577}/>
+      <img src="/image/carro.jpg" alt="Bugatti 3" />
+      <Image src='https://assegurou.com.br/blog/wp-content/uploads/2024/03/Bugatti-La-Voiture-Noire-1024x577.jpg' alt="Bugatti 4" width={1024} height={577}/>
     </div>
   );
 }
