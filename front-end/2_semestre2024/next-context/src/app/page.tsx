9a3413cb28d34e51ce453e20e0701189f6bba542
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2024-10-30 08:18:08 -0300

    next-context

---
+++ front-end/2_semestre2024/next-context/src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+   <main className="flex justify-center p-10">
+      <h1 className="text-center text-4xl font-bold text-indigo-800">Home</h1>
+   </main>
+  );
+}
