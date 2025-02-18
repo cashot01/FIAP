ad530b0679f9490c2d3a4ec947e8e58a7b428915
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-02-18 11:04:59 -0300

    vinteum

---
+++ 2025/java/vinteum/src/main/java/com/example/Jogo.java
@@ -0,0 +1,15 @@
+package com.exemple;
+
+public class Jogo {
+
+    protected Monte monte = new Monte();
+    
+
+
+
+
+
+
+
+    
+}
--- 2025/java/vinteum/src/main/java/com/example/Monte.java
+++ 2025/java/vinteum/src/main/java/com/example/Monte.java
@@ -1,5 +1,18 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Monte {
+
+    private List<Carta> cartas = new ArrayList<>();
+
+    public Monte(){
+        for(int numero = 0; numero <= 13; numero++){
+            for (Naipe naipe : Naipe.values()){
+                cartas.add( new Carta(numero, naipe));
+            }
+        }
+    }
 
 }
