d2a21a6ae84421a583f9bc33ddc1f710a77dc319
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-02-25 11:41:05 -0300

    controller category + dependecia devTools

---
+++ 2025/java/cash-up-api/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
@@ -0,0 +1,17 @@
+package br.com.fiap.cash_up_api.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // component
+public class CategoryController {
+
+    // listar todas as categorias
+    // GET :8080/categories -> json
+    @RequestMapping(method = {RequestMethod.GET}, path = "/categories", produces = "application/json")
+    public String index(){
+        return "Lista de categorias";
+    }
+}
