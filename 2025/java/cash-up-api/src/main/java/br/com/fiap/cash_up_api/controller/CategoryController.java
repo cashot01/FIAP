5247e9433aa19985b47c5fa8e2762eb668fe6486
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-02-25 11:57:41 -0300

    catogory no model

--- 2025/java/cash-up-api/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
+++ 2025/java/cash-up-api/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
@@ -1,17 +1,29 @@
 package br.com.fiap.cash_up_api.controller;
 
 
+import br.com.fiap.cash_up_api.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController // component
 public class CategoryController {
 
     // listar todas as categorias
     // GET :8080/categories -> json
-    @RequestMapping(method = {RequestMethod.GET}, path = "/categories", produces = "application/json")
-    public String index(){
-        return "Listar todas as categorias";
+    @GetMapping("/categories")
+    public List<Category> index(){
+        return List.of(
+                new Category(1L, "Educação", "book"),
+                new Category(2L, "Lazer", "casino"),
+                new Category(3L, "Salário", "payments")
+        );
     }
+
+    // cadastrar categoria
+
+    // apagar categoria
 }
