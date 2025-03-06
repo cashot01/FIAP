984c752a76f150c23ae6f832b23a1955efee2610
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-06 11:21:51 -0300

    banco

---
+++ 2025/banco/modeloPedido.sql
@@ -0,0 +1,14 @@
+/* baixar arquivo SCRIPT MODELO PEDIDO no portal do aluno */
+
+@ 'C:\Users\labsfiap\Documents\GitHub\FIAP\2025\banco\SCRIPT_MODELO_PEDIDO(1).SQL';
+
+INSERT into pais SELECT * from pf1788.pais;
+commit;
+
+INSERT into estado SELECT * from pf1788.estado;
+commit;
+
+INSERT into cidade SELECT * from pf1788.cidade;
+commit;
+
+ALTER TABLE cidade MODIFY nom_cidade VARCHAR2(30);
