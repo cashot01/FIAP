107a2ef86bd96f8780ec0a0647fe821fd00deb69
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-06 10:22:40 -0300

    banco 06/03

---
+++ 2025/banco/aula06-03.sql
@@ -0,0 +1,21 @@
+/*
+
+Loop
+	< instrução(ões) >
+	Exit when < condição >
+End loop;
+
+
+*/
+
+set serveroutput on;
+
+DECLARE 
+	V_CONTADOR NUMBER(2):= 1; 
+BEGIN 
+LOOP 	
+	DBMS_OUTPUT.PUT_LINE(V_CONTADOR); 
+	V_CONTADOR := V_CONTADOR + 1; 
+	EXIT WHEN V_CONTADOR > 20; 
+END LOOP; 
+END;
