49edc7375333ea44e6da023824a7d78d36a23a40
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-20 09:00:10 -0300

    cash up

--- 2025/java/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/model/Category.java
+++ 2025/java/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/model/Category.java
@@ -2,7 +2,12 @@ package br.com.fiap.cash_up_api.model;
 
 import java.util.Random;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
     private Long id;
     private String name;
     private String icon;
