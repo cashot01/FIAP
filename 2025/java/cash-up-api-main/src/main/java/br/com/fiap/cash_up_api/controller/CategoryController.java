9dc2e5e1408c9417285d07fcc6b218ca6b2162ec
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-03-20 09:47:48 -0300

    projeto cash up melhorado

--- 2025/java/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
+++ 2025/java/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -21,6 +23,7 @@ import br.com.fiap.cash_up_api.model.Category;
 import br.com.fiap.cash_up_api.repository.CategoryRepository;
 
 @RestController // component
+@RequestMapping("/categories")
 public class CategoryController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
@@ -28,42 +31,43 @@ public class CategoryController {
     @Autowired // injeção de dependencia
     private CategoryRepository repository;
 
-    @GetMapping("/categories")
+    @GetMapping
     public List<Category> index() {
         log.info("Buscando todas categorias");
         return repository.findAll();
     }
 
-    @PostMapping("/categories")
-    public ResponseEntity<Category> create(@RequestBody Category category) {
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category create(@RequestBody Category category) {
         log.info("Cadastrando categoria " + category.getName());
         repository.save(category);
         // trocou o add pelo save
-        return ResponseEntity.status(201).body(category);
+        return category;
     }
 
-    @GetMapping("/categories/{id}")
-    public ResponseEntity<Category> get(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public Category get(@PathVariable Long id) {
         log.info("Buscando categoria " + id);
-        return ResponseEntity.ok(getCategory(id));
+        return getCategory(id);
     }
 
-    @DeleteMapping("/categories/{id}")
-    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void destroy(@PathVariable Long id) {
         log.info("Apagando categoria " + id);
         repository.delete(getCategory(id));
         // trocou remove -> delete
-        return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/categories/{id}")
-    public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category category) {
+    @PutMapping("/{id}")
+    public Category update(@PathVariable Long id, @RequestBody Category category) {
         log.info("Atualizando categoria " + id + " " + category);
 
         getCategory(id);
         category.setId(id);
-        repository.save(category);
-        return ResponseEntity.ok(category);
+        return repository.save(category);
+        
     }
 
     private Category getCategory(Long id) {
