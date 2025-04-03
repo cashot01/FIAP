645749936f302d6d13300f828078a00c5157c1fd
Cauan Aranega <110852153+cashot01@users.noreply.github.com>
2025-04-03 08:23:26 -0300

    update cashUp API

--- 2025/java/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
+++ 2025/java/cash-up-api-main/src/main/java/br/com/fiap/cash_up_api/controller/CategoryController.java
@@ -1,13 +1,12 @@
 package br.com.fiap.cash_up_api.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,14 +20,15 @@ import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.cash_up_api.model.Category;
 import br.com.fiap.cash_up_api.repository.CategoryRepository;
+import jakarta.validation.Valid;
 
-@RestController // component
+@RestController
 @RequestMapping("/categories")
 public class CategoryController {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @Autowired // injeção de dependencia
+    @Autowired
     private CategoryRepository repository;
 
     @GetMapping
@@ -39,35 +39,31 @@ public class CategoryController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Category create(@RequestBody Category category) {
+    public Category create(@RequestBody @Valid Category category) {
         log.info("Cadastrando categoria " + category.getName());
-        repository.save(category);
-        // trocou o add pelo save
-        return category;
+        return repository.save(category);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("{id}")
     public Category get(@PathVariable Long id) {
         log.info("Buscando categoria " + id);
         return getCategory(id);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void destroy(@PathVariable Long id) {
         log.info("Apagando categoria " + id);
         repository.delete(getCategory(id));
-        // trocou remove -> delete
     }
 
-    @PutMapping("/{id}")
-    public Category update(@PathVariable Long id, @RequestBody Category category) {
+    @PutMapping("{id}")
+    public Category update(@PathVariable Long id, @RequestBody @Valid Category category) {
         log.info("Atualizando categoria " + id + " " + category);
 
         getCategory(id);
         category.setId(id);
         return repository.save(category);
-        
     }
 
     private Category getCategory(Long id) {
@@ -76,7 +72,6 @@ public class CategoryController {
                         () -> new ResponseStatusException(
                                 HttpStatus.NOT_FOUND,
                                 "Categoria não encontrada"));
-                // trocou o stream pelo findById
     }
 
 }
